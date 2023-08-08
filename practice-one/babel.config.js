98562af5b26d339a233a13774da418252218af73
Toan <toan124677@gmail.com>
2023-08-08 23:58:17 +0700

    Set up alias path and folder structure

--- practice-one/babel.config.js
+++ practice-one/babel.config.js
@@ -1,6 +1,26 @@
-module.exports = function(api) {
-  api.cache(true);
+module.exports = function (api) {
+  api.cache(true)
   return {
     presets: ['babel-preset-expo'],
-  };
-};
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./app'],
+          extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+          alias: {
+            '@components': './app/components',
+            '@screens': './app/screens',
+            '@assets': './assets',
+            '@constants': './app/constants',
+            '@utils': './app/utils',
+            '@types': './app/types',
+            '@hooks': './app/hooks',
+            '@navigation': './app/navigation',
+            '@services': './app/services',
+          },
+        },
+      ],
+    ],
+  }
+}
