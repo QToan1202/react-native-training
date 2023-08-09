b2840e58e4b93b444d7659966a1a82f80fd2b39c
Toan <toan124677@gmail.com>
2023-08-09 15:28:55 +0700

    Load custom fonts for storybook

--- practice-one/.storybook/index.tsx
+++ practice-one/.storybook/index.tsx
@@ -2,6 +2,22 @@ import { getStorybookUI } from '@storybook/react-native'
 
 import './storybook.requires'
 
+import {
+  Montserrat_400Regular,
+  Montserrat_500Medium,
+  Montserrat_600SemiBold,
+  Montserrat_700Bold,
+} from '@expo-google-fonts/montserrat'
+import * as Font from 'expo-font'
+import * as SplashScreen from 'expo-splash-screen'
+
+Font.loadAsync({
+  Montserrat_400Regular,
+  Montserrat_500Medium,
+  Montserrat_600SemiBold,
+  Montserrat_700Bold,
+}).then(() => SplashScreen.hideAsync())
+
 const StorybookUIRoot = getStorybookUI({})
 
 export default StorybookUIRoot
