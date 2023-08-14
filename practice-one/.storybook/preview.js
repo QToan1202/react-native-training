d6645b641c061f8bfef3d1a960ddd768a8d3d02f
Toan <toan124677@gmail.com>
2023-08-14 10:04:03 +0700

    Install background package for storybook

--- practice-one/.storybook/preview.js
+++ practice-one/.storybook/preview.js
@@ -1,8 +1,21 @@
+import { withBackgrounds } from '@storybook/addon-ondevice-backgrounds'
+import { View } from 'react-native'
+
+export const decorators = [withBackgrounds]
+
 export const parameters = {
+  backgrounds: {
+    default: 'light',
+    values: [
+      { name: 'light', value: '#fff' },
+      { name: 'dark', value: '#000' },
+    ],
+  },
+
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-};
+}
