df56e9f05fdc418bd7c51862c306030841e9ce25
Toan <toan124677@gmail.com>
2023-08-17 11:17:10 +0700

    Update search stories

--- practice-one/app/components/Search/Search.stories.tsx
+++ practice-one/app/components/Search/Search.stories.tsx
@@ -1,21 +1,13 @@
-import { useState } from 'react'
 import { ComponentMeta, ComponentStory } from '@storybook/react-native'
 
 import { Search } from '../index'
-import { SearchProps } from './index'
 
 export default {
   title: 'Search',
   component: Search,
 } as ComponentMeta<typeof Search>
 
-const Template: ComponentStory<typeof Search> = (
-  args: Omit<SearchProps, 'value' | 'onChangeText'>
-) => {
-  const [value, onChangeValue] = useState<string>('')
-
-  return <Search value={value} onChangeText={onChangeValue} {...args} />
-}
+const Template: ComponentStory<typeof Search> = (args) => <Search {...args} />
 
 export const Default = Template.bind({})
 Default.args = {
