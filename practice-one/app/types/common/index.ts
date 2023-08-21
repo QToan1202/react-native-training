2857533b118e0fa64aa5ce35e6c7d9253bfab1e8
Toan <toan124677@gmail.com>
2023-08-21 09:21:14 +0700

    Create icon list interface

--- practice-one/app/types/common/index.ts
+++ practice-one/app/types/common/index.ts
@@ -1,3 +1,5 @@
+import { ImageProps } from 'react-native'
+
 import { IUserForms } from '../users'
 
 export interface IInputSearch {
@@ -9,3 +11,9 @@ export interface IForm extends IInputSearch, IUserForms {}
 export type TButtonVariant = 'primary' | 'secondary' | 'tertiary' | 'quaternary'
 
 export type TSize = 'sm' | 'md' | 'lg' | 'xl'
+
+export interface IIconList {
+  label: string
+  icon: ImageProps['source']
+  action: () => void
+}
