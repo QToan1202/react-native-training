8438c91fae339f6f6dbef01ce26fc9dde31c8cd2
Toan <toan124677@gmail.com>
2023-08-21 09:21:50 +0700

    Create container and image styles for re-using

---
+++ practice-one/app/styles/container/index.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native'
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 16,
+    paddingVertical: 22,
+  },
+  inline: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    columnGap: 4,
+  },
+})
+
+export default styles
---
+++ practice-one/app/styles/index.ts
@@ -0,0 +1,2 @@
+export { default as containerStyles } from './container'
+export { default as imageStyles } from './image'
