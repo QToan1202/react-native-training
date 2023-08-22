4e84543fa53c23cc22519cdba4b801e7a69a0a53
Toan <toan124677@gmail.com>
2023-08-22 15:48:34 +0700

    Add custom style and extend props for TabIcon component

--- practice-one/app/components/TabIcon/index.tsx
+++ practice-one/app/components/TabIcon/index.tsx
@@ -1,54 +1,60 @@
-import { Image, ImageStyle } from 'react-native'
+import { Image, ImageProps, ImageStyle, StyleProp } from 'react-native'
 
 import { imageStyles } from '@styles'
 
-export interface IconProps {
+export interface IconProps extends Omit<ImageProps, 'source'> {
   color: string
+  style?: StyleProp<ImageStyle>
 }
 
 const setIconColor = (color: string): ImageStyle => ({ tintColor: color })
 
-const Home = ({ color }: IconProps) => {
+const Home = ({ color, style, ...rest }: IconProps) => {
   return (
     <Image
       source={require('@assets/tab/home.png')}
-      style={[imageStyles.icon, setIconColor(color)]}
+      style={[imageStyles.icon, setIconColor(color), style]}
+      {...rest}
     />
   )
 }
 
-const Order = ({ color }: IconProps) => {
+const Order = ({ color, style, ...rest }: IconProps) => {
   return (
     <Image
       source={require('@assets/tab/order.png')}
-      style={[imageStyles.icon, setIconColor(color)]}
+      style={[imageStyles.icon, setIconColor(color), style]}
+      {...rest}
     />
   )
 }
 
-const Profile = ({ color }: IconProps) => {
+const Profile = ({ color, style, ...rest }: IconProps) => {
   return (
     <Image
       source={require('@assets/tab/profile.png')}
-      style={[imageStyles.icon, setIconColor(color)]}
+      style={[imageStyles.icon, setIconColor(color), style]}
+      {...rest}
     />
   )
 }
 
-const Search = ({ color }: IconProps) => {
+const Search = ({ color, style, ...rest }: IconProps) => {
   return (
     <Image
       source={require('@assets/tab/search.png')}
-      style={[imageStyles.icon, setIconColor(color)]}
+      style={[imageStyles.icon, setIconColor(color), style]}
+      {...rest}
     />
   )
 }
 
-const Store = ({ color }: IconProps) => {
+const Store = ({ color, style, ...rest }: IconProps) => {
   return (
     <Image
       source={require('@assets/tab/store.png')}
-      style={[imageStyles.icon, setIconColor(color)]}
+      style={[imageStyles.icon, setIconColor(color), style]}
+      {...rest}
     />
   )
 }
