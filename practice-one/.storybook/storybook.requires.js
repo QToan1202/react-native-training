1d05ed2e67127d78631b9d389fef577a6ec07c6f
Toan <toan124677@gmail.com>
2023-08-22 23:57:05 +0700

    #2 Create store card component

--- practice-one/.storybook/storybook.requires.js
+++ practice-one/.storybook/storybook.requires.js
@@ -64,6 +64,7 @@ const getStories = () => {
     "./app/components/Paragraph/Paragraph.stories.tsx": require("../app/components/Paragraph/Paragraph.stories.tsx"),
     "./app/components/Picker/Picker.stories.tsx": require("../app/components/Picker/Picker.stories.tsx"),
     "./app/components/Search/Search.stories.tsx": require("../app/components/Search/Search.stories.tsx"),
+    "./app/components/StoreCard/StoreCard.stories.tsx": require("../app/components/StoreCard/StoreCard.stories.tsx"),
     "./app/navigation/Tab/Tab.stories.tsx": require("../app/navigation/Tab/Tab.stories.tsx"),
   };
 };
