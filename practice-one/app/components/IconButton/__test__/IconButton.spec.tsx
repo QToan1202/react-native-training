fc0cc527639818a79aa86f6e4231d78c3fb5df65
Toan <toan124677@gmail.com>
2023-08-23 23:33:11 +0700

    Update icon button component test

--- practice-one/app/components/IconButton/__test__/IconButton.spec.tsx
+++ practice-one/app/components/IconButton/__test__/IconButton.spec.tsx
@@ -1,4 +1,3 @@
-import { Image } from 'react-native'
 import { render, fireEvent } from '@testing-library/react-native'
 
 import IconButton from '../index'
@@ -7,9 +6,7 @@ describe('Testing icon button component', () => {
   it('Render button component with an icon', () => {
     const mockOnPress = jest.fn()
     const component = render(
-      <IconButton onPress={mockOnPress} noBackground>
-        <Image source={require('@assets/favicon.png')} />
-      </IconButton>
+      <IconButton icon={require('@assets/favicon.png')} onPress={mockOnPress} noBackground />
     ).toJSON()
 
     expect(component).toMatchSnapshot()
@@ -20,9 +17,11 @@ describe('Testing icon button component', () => {
     mockOnPress.mockReturnValue(true)
 
     const { getByLabelText } = render(
-      <IconButton onPress={mockOnPress} accessibilityLabel="icon button">
-        <Image source={require('@assets/favicon.png')} />
-      </IconButton>
+      <IconButton
+        icon={require('@assets/favicon.png')}
+        onPress={mockOnPress}
+        accessibilityLabel="icon button"
+      />
     )
 
     fireEvent.press(getByLabelText('icon button'))
