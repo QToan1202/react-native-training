9cac54b2187a413bfaeee5e7cf534bbee968cca4
Toan <toan124677@gmail.com>
2023-08-30 12:05:23 +0700

    Update storybook to get stories from screens folder

--- practice-one/.storybook/main.js
+++ practice-one/.storybook/main.js
@@ -2,6 +2,7 @@ module.exports = {
   stories: [
     '../app/components/**/*.stories.?(ts|tsx|js|jsx)',
     '../app/navigation/**/*.stories.?(ts|tsx|js|jsx)',
+    '../app/screens/**/*.stories.?(ts|tsx|js|jsx)',
   ],
   addons: [
     '@storybook/addon-ondevice-controls',
