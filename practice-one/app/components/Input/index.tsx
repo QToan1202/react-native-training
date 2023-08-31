a574a167ced203335dbc75f133938ca48977db1b
Toan <toan124677@gmail.com>
2023-08-31 11:32:10 +0700

    Provide validation rules and overriding style input

--- practice-one/app/components/Input/index.tsx
+++ practice-one/app/components/Input/index.tsx
@@ -1,6 +1,6 @@
 import { forwardRef, useCallback, useState } from 'react'
 import { TextInput, TextInputProps } from 'react-native'
-import { Control, useController } from 'react-hook-form'
+import { Control, UseControllerProps, useController } from 'react-hook-form'
 
 import Paragraph from '@components/Paragraph'
 import { IForm } from '@types'
@@ -11,41 +11,46 @@ export interface InputProps extends TextInputProps {
   label?: string
   name: keyof IForm
   control: Control<IForm>
+  rules?: UseControllerProps['rules']
 }
 
-const Input = forwardRef<TextInput, InputProps>(({ label, name, control, ...rest }, ref) => {
-  const { field } = useController<IForm>({
-    control,
-    defaultValue: '',
-    name,
-  })
-  const [isFocus, setIsFocus] = useState(false)
-  const handleFocus = useCallback(() => setIsFocus(true), [])
-  const handleBlur = useCallback(() => setIsFocus(false), [])
+const Input = forwardRef<TextInput, InputProps>(
+  ({ label, name, control, rules, style, ...rest }, ref) => {
+    const { field } = useController<IForm>({
+      control,
+      defaultValue: '',
+      name,
+      rules,
+    })
+    const [isFocus, setIsFocus] = useState(false)
+    const handleFocus = useCallback(() => setIsFocus(true), [])
+    const handleBlur = useCallback(() => setIsFocus(false), [])
 
-  return (
-    <>
-      {label && (
-        <Paragraph
-          size="sm"
-          content={label}
-          style={isFocus ? [styles.label, styles.labelFocus] : styles.label}
+    return (
+      <>
+        {label && (
+          <Paragraph
+            size="sm"
+            content={label}
+            style={isFocus ? [styles.label, styles.labelFocus] : styles.label}
+          />
+        )}
+        <TextInput
+          ref={ref}
+          style={[
+            label ? styles.inputWithLabel : styles.input, // Style when an input have label or not
+            isFocus && styles.inputFocus, // Change style of the input that have label when focus
+            style,
+          ]}
+          value={String(field.value)}
+          onChangeText={field.onChange}
+          onFocus={handleFocus}
+          onBlur={handleBlur}
+          {...rest}
         />
-      )}
-      <TextInput
-        ref={ref}
-        style={[
-          label ? styles.inputWithLabel : styles.input, // Style when an input have label or not
-          isFocus && styles.inputFocus, // Change style of the input that have label when focus
-        ]}
-        value={field.value.toString()}
-        onChangeText={field.onChange}
-        onFocus={handleFocus}
-        onBlur={handleBlur}
-        {...rest}
-      />
-    </>
-  )
-})
+      </>
+    )
+  }
+)
 
 export default Input
