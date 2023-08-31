2e18b3dec818a8ee300f1baa4f247dc3f003b844
Toan <toan124677@gmail.com>
2023-08-31 11:32:38 +0700

    Remove text transform in heading styles

--- practice-one/app/components/Heading/styles.ts
+++ practice-one/app/components/Heading/styles.ts
@@ -6,7 +6,6 @@ const styles = StyleSheet.create({
   heading_bold: {
     fontFamily: FONT_FAMILY.MONTSERRAT[700],
     fontSize: 24,
-    textTransform: 'capitalize',
     color: COLORS.WHITE,
   },
   heading_medium: {
