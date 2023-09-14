874d6e139293b57bb17e3e23a7eddebca4485203
Toan <toan124677@gmail.com>
2023-09-14 11:00:54 +0700

    Add an override onPress for renderItem util function

--- practice-one/app/utils/render/index.tsx
+++ practice-one/app/utils/render/index.tsx
@@ -3,8 +3,10 @@ import { ListRenderItemInfo } from 'react-native'
 import { IFlatListBase } from '@types'
 
 const renderItem =
-  <T extends IFlatListBase>(Element: React.JSX.ElementType) =>
+  <T extends IFlatListBase>(Element: React.JSX.ElementType, action?: (data: T) => void) =>
   // eslint-disable-next-line react/function-component-definition
-  ({ item: { ...rest } }: ListRenderItemInfo<T>) => <Element {...rest} />
+  ({ item: { ...rest } }: ListRenderItemInfo<T>) => (
+    <Element {...rest} onPress={() => action && action({ ...rest })} />
+  )
 
 export default renderItem
