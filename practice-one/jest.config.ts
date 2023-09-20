dd95c3ad025aefda203115d710947f74e42be747
Toan <toan124677@gmail.com>
2023-09-20 11:59:02 +0700

    Split jest config to new file

---
+++ practice-one/jest.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from 'jest'
+
+const config: Config = {
+  preset: 'react-native',
+  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+  transformIgnorePatterns: [
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)',
+  ],
+  setupFiles: ['./setupTests.ts'],
+  moduleDirectories: ['node_modules', 'utils', './app'],
+}
+
+export default config
