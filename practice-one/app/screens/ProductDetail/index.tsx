769605dfa09ae663fb6ffb85b6c9af4d610244dc
Toan <toan124677@gmail.com>
2023-09-20 10:11:04 +0700

    Optimal duplicate code

--- practice-one/app/screens/ProductDetail/index.tsx
+++ practice-one/app/screens/ProductDetail/index.tsx
@@ -1,10 +1,12 @@
-import { useCallback } from 'react'
+import React, { useCallback, useMemo } from 'react'
 import { ImageBackground, ScrollView, View } from 'react-native'
 import { NativeStackScreenProps } from '@react-navigation/native-stack'
 import { RootStackParamList } from '@navigation/Stack'
 
 import { Avatar, Button, Heading, IconButton, Paragraph } from '@components'
 import { containerStyles } from '@styles'
+import { CATEGORY } from '@constants'
+
 import styles from './styles'
 
 export interface ProductDetailProps
@@ -13,6 +15,13 @@ export interface ProductDetailProps
 const ProductDetail = ({ navigation }: ProductDetailProps) => {
   const handleBackPress = useCallback(() => navigation.goBack(), [navigation])
   const handlePress = useCallback(() => undefined, [])
+  const Categories: React.JSX.Element[] = useMemo(
+    () =>
+      CATEGORY.map((item: string) => (
+        <Paragraph style={[styles.text, styles.textTransform]} content={item} />
+      )),
+    []
+  )
 
   return (
     <ScrollView contentContainerStyle={styles.container} showsVerticalScrollIndicator={false}>
@@ -47,44 +56,7 @@ const ProductDetail = ({ navigation }: ProductDetailProps) => {
         <Paragraph
           content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lobortis cras placerat diam ipsum ut. Nisi vel adipiscing massa bibendum diam. Suspendisse mattis dui maecenas duis mattis. Mattis aliquam at arcu, semper nunc, venenatis et pellentesque eu. Id tristique maecenas tristique habitasse eu elementum sed. Aliquam eget lacus, arcu, adipiscing eget feugiat in dolor sagittis.
 Non commodo, a justo massa porttitor sed placerat in. Orci tristique etiam tempus sed. Mi varius morbi egestas dictum tempor nisl. In "
-          style={styles.text}
-        />
-      </View>
-      <View style={styles.contentWrapper}>
-        <Heading style={[styles.text, styles.contentTitle]} content="Details" />
-        <View style={[containerStyles.inline, styles.detailWrapper]}>
-          <View style={[styles.category, styles.spacing]}>
-            <Paragraph style={[styles.text, styles.textTransform]} content="condition" />
-            <Paragraph style={[styles.text, styles.textTransform]} content="price type" />
-            <Paragraph style={[styles.text, styles.textTransform]} content="category" />
-            <Paragraph style={[styles.text, styles.textTransform]} content="location" />
-          </View>
-          <View style={[styles.categoryInfo, styles.spacing]}>
-            <Paragraph style={[styles.text, styles.textTransform]} content="organic" />
-            <Paragraph style={[styles.text, styles.textTransform]} content="fixed" />
-            <Paragraph style={[styles.text, styles.textTransform]} content="beverages" />
-            <Paragraph
-              style={[styles.text, styles.textTransform]}
-              content="kualalumpur, malaysia"
-            />
-          </View>
-        </View>
-      </View>
-      <View style={styles.contentWrapper}>
-        <Heading style={[styles.text, styles.contentTitle]} content="Additional Details" />
-        <View style={[containerStyles.inline, styles.detailWrapper]}>
-          <View style={[styles.category, styles.spacing]}>
-            <Paragraph style={[styles.text, styles.textTransform]} content="delivery details" />
-          </View>
-          <View style={[styles.categoryInfo, styles.spacing]}>
-            <Paragraph
-              style={[styles.text, styles.textTransform]}
-              content="Home Delivery Available, Cash On Delivery"
-            />
-          </View>
-        </View>
-      </View>
-    </ScrollView>
+            <View style={[styles.category, styles.spacing]}>{Categories}</View>
   )
 }
 
