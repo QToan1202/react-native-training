0c08161899a05aa38b95fcfaaed3cb66574994c0
Toan <toan124677@gmail.com>
2023-09-21 12:03:52 +0700

    Add decorator for CategoryDetail screen

--- practice-one/app/screens/CategoryDetail/CategoryDetail.stories.tsx
+++ practice-one/app/screens/CategoryDetail/CategoryDetail.stories.tsx
@@ -1,13 +1,14 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react-native'
 
+import { NavigationDecorator } from '@utils/StoryNavigator'
+
 import { CategoryDetail } from '../index'
 
 export default {
   title: 'screens/Category Detail',
   component: CategoryDetail,
+  decorators: [NavigationDecorator],
 } as ComponentMeta<typeof CategoryDetail>
 
-const Template: ComponentStory<typeof CategoryDetail> = () => <CategoryDetail />
-
+const Template: ComponentStory<typeof CategoryDetail> = (args) => <CategoryDetail {...args} />
 export const Default = Template.bind({})
-Default.args = {}
