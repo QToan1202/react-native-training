a178497334845510c291afe424d0a3d198bf1922
Toan <toan124677@gmail.com>
2023-09-25 10:31:21 +0700

    Update props for CategoryDetail screen stories

--- practice-one/app/screens/CategoryDetail/CategoryDetail.stories.tsx
+++ practice-one/app/screens/CategoryDetail/CategoryDetail.stories.tsx
@@ -1,6 +1,9 @@
+import { RouteProp } from '@react-navigation/native'
+import { NativeStackNavigationProp } from '@react-navigation/native-stack'
 import { ComponentMeta, ComponentStory } from '@storybook/react-native'
 
 import { NavigationDecorator } from '@utils/StoryNavigator'
+import { RootStackParamList } from '@navigation/Stack'
 
 import { CategoryDetail } from '../index'
 
@@ -10,5 +13,20 @@ export default {
   decorators: [NavigationDecorator],
 } as ComponentMeta<typeof CategoryDetail>
 
+const navigation = {
+  setOptions: () => ({
+    headTitle: 'mock header',
+  }),
+} as unknown as NativeStackNavigationProp<RootStackParamList, 'CategoryDetail'>
+
+const route = {
+  name: 'CategoryDetail',
+  params: { id: '1', name: 'header name' },
+} as unknown as RouteProp<RootStackParamList, 'CategoryDetail'>
+
 const Template: ComponentStory<typeof CategoryDetail> = (args) => <CategoryDetail {...args} />
 export const Default = Template.bind({})
+Default.args = {
+  navigation,
+  route,
+}
