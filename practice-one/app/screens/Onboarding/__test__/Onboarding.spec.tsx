81bf0349fa922b66f9de452aa1994c1b9d63559d
Toan <toan124677@gmail.com>
2023-09-25 10:03:46 +0700

    Update fail test cases

--- practice-one/app/screens/Onboarding/__test__/Onboarding.spec.tsx
+++ practice-one/app/screens/Onboarding/__test__/Onboarding.spec.tsx
@@ -1,22 +1,28 @@
+import { NativeStackNavigationProp } from '@react-navigation/native-stack'
 import { fireEvent, screen } from '@testing-library/react-native'
+import { RouteProp } from '@react-navigation/native'
+
 import { render } from 'test-utils'
+import { RootStackParamList } from '@navigation/Stack'
 
 import Onboarding from '../index'
 
-const props = {
-  navigation: {
-    navigate: jest.fn(),
-    goBack: jest.fn(),
-  },
-}
+const navigation = {
+  navigate: jest.fn(),
+  goBack: jest.fn(),
+} as unknown as NativeStackNavigationProp<RootStackParamList, 'Onboarding'>
+
+const route = {
+  name: 'Onboarding',
+} as unknown as RouteProp<RootStackParamList, 'Onboarding'>
 
 describe('Testing onboarding screen', () => {
   beforeEach(() => {
-    render(<Onboarding {...props} testID="viewpager" />)
+    render(<Onboarding navigation={navigation} route={route} testID="viewpager" />)
   })
 
   it('Render screen successfully', () => {
-    const component = render(<Onboarding {...props} />).toJSON()
+    const component = render(<Onboarding navigation={navigation} route={route} />).toJSON()
 
     expect(component).toMatchSnapshot()
   })
@@ -31,6 +37,6 @@ describe('Testing onboarding screen', () => {
     expect(finishBtn).toBeOnTheScreen()
 
     fireEvent.press(finishBtn)
-    expect(props.navigation.navigate).toBeCalledTimes(1)
+    expect(navigation.navigate).toBeCalledTimes(1)
   })
 })
