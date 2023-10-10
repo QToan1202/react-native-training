cc3cc504082b688f20123e8518ef4ffb86ed1ab5
Toan <toan124677@gmail.com>
2023-10-10 15:28:47 +0700

    Update generic and provide callback for asyncStorage override action

--- practice-one/app/services/asyncStorage/index.ts
+++ practice-one/app/services/asyncStorage/index.ts
@@ -2,28 +2,29 @@ import AsyncStorage from '@react-native-async-storage/async-storage'
 
 import { TAsyncStoreReturnType } from '@types'
 
-const save = async (key: string, value: string | object) => {
+const save = async (key: string, value: string | object, onComplete?: () => void) => {
   try {
     const saveValue: string = JSON.stringify(value)
 
-    await AsyncStorage.setItem(key, saveValue)
+    await AsyncStorage.setItem(key, saveValue, onComplete)
   } catch (e) {
     if (e instanceof Error) throw new Error(e.message)
   }
 }
 
-const get = async (
+const get = async <T>(
   key: string,
+  onComplete?: () => void,
   returnType: TAsyncStoreReturnType = 'string'
-): Promise<string | object | Error> => {
+): Promise<T> => {
   try {
-    const value: string | null = await AsyncStorage.getItem(key)
+    const value: string | null = await AsyncStorage.getItem(key, onComplete)
 
     if (value === null) throw new Error('Null value')
 
-    if (returnType === 'object') JSON.parse(value)
+    if (returnType === 'object') return JSON.parse(value)
 
-    return value
+    return value as T
   } catch (e) {
     if (e instanceof Error) throw new Error(e.message)
 
