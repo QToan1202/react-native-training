baf9a1b4bb06f878c602feb3162cb07de8f1c3d0
Toan <92291488+QToan02@users.noreply.github.com>
2023-11-15 11:57:31 +0700

    #13 Refactor Address component

--- practice-two/app/components/Address/index.tsx
+++ practice-two/app/components/Address/index.tsx
@@ -1,31 +1,45 @@
 import { memo } from 'react'
 import isEqual from 'react-fast-compare'
-import { View, ViewProps } from 'react-native'
+import { XStack, XStackProps, YStack, getTokens } from 'tamagui'
 
 import Button from '@components/Button'
 import Paragraph from '@components/Paragraph'
-import { containerStyles } from '@styles'
 
-import styles from './styles'
-
-export interface AddressProps extends ViewProps {
+export type AddressProps = {
   name: string
   streetAddress: string
   onPress: () => void
-}
+} & XStackProps
 
-const Address = ({ name, streetAddress, style, onPress, ...rest }: AddressProps) => {
+const Address = ({ name, streetAddress, onPress, ...rest }: AddressProps) => {
   return (
-    <View
-      style={[containerStyles.inline, containerStyles.spaceBetween, styles.container, style]}
+    <XStack
+      justifyContent="space-between"
+      alignItems="center"
+      space={4}
+      paddingVertical={15}
+      paddingHorizontal={20}
+      backgroundColor="$color.white"
       {...rest}
     >
-      <View style={styles.spacing}>
-        <Paragraph style={styles.text} content={name} />
-        <Paragraph style={[styles.text, styles.address]} content={streetAddress} />
-      </View>
-      <Button style={styles.btn} titleStyle={styles.btnTitle} title="change" onPress={onPress} />
-    </View>
+      <YStack space={4}>
+        <Paragraph fontWeight="$2" color="$gray_50" lineHeight={20} content={name} />
+        <Paragraph
+          fontWeight="$2"
+          color="$gray_50"
+          lineHeight={20}
+          opacity={0.7}
+          content={streetAddress}
+        />
+      </YStack>
+      <Button
+        paddingVertical={4}
+        paddingHorizontal={getTokens().space[5].val - 1}
+        onPress={onPress}
+        fontSize="$1"
+        title="Change"
+      />
+    </XStack>
   )
 }
 
