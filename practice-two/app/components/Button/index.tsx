b1521f7a3717188af5e80a72426ee57adcb02e7b
Toan <92291488+QToan02@users.noreply.github.com>
2023-11-15 11:18:21 +0700

    #13 Refactor Button component

--- practice-two/app/components/Button/index.tsx
+++ practice-two/app/components/Button/index.tsx
@@ -1,76 +1,32 @@
-import { memo, useMemo } from 'react'
+import { memo } from 'react'
+import { Image, ImageSourcePropType } from 'react-native'
 import isEqual from 'react-fast-compare'
-import {
-  Image,
-  ImageProps,
-  StyleProp,
-  TextStyle,
-  TouchableOpacity,
-  TouchableOpacityProps,
-  View,
-  ViewStyle,
-} from 'react-native'
 
-import { TButtonVariant } from '@types'
-import Paragraph from '@components/Paragraph'
 import { imageStyles } from '@styles'
-import { COLORS } from '@constants'
 
-import styles from './styles'
+import ButtonStyled, { ButtonProps as StyledButtonProps } from './styles'
 
-export interface ButtonProps extends TouchableOpacityProps {
+export interface ButtonProps extends StyledButtonProps {
   title: string
-  leftIcon?: ImageProps['source']
-  rightIcon?: ImageProps['source']
-  variant?: TButtonVariant
-  shrink?: boolean
-  style?: StyleProp<ViewStyle>
-  titleStyle?: StyleProp<TextStyle>
-  onPress: () => void
+  leftIcon?: ImageSourcePropType
+  rightIcon?: ImageSourcePropType
 }
 
-const Button = ({
-  title,
-  leftIcon,
-  rightIcon,
-  variant = 'primary',
-  shrink = false,
-  style,
-  titleStyle,
-  onPress,
-  ...rest
-}: ButtonProps) => {
-  const titleVariantStyle = useMemo((): TextStyle => {
-    switch (variant) {
-      case 'primary':
-        return { color: COLORS.WHITE }
-      case 'secondary':
-        return { color: COLORS.PRIMARY }
-      case 'tertiary':
-        return { color: COLORS.WHITE }
-      case 'quaternary':
-        return { color: COLORS.BLACK }
-      default:
-        return { color: COLORS.WHITE }
-    }
-  }, [variant])
-
+const Button = ({ title, leftIcon, rightIcon, ...rest }: ButtonProps) => {
   return (
-    <TouchableOpacity
-      style={[styles.btn, styles[variant], shrink && styles.btnSmall, style]}
-      onPress={onPress}
-      {...rest}
-    >
-      <View style={styles.btnContent}>
-        {leftIcon && <Image source={leftIcon} style={imageStyles.icon} />}
-        <Paragraph
-          size="md"
-          style={[titleVariantStyle, styles.btnTitle, titleStyle]}
-          content={title}
-        />
-        {rightIcon && <Image source={rightIcon} style={imageStyles.icon} />}
-      </View>
-    </TouchableOpacity>
+    <ButtonStyled {...rest}>
+      {leftIcon && (
+        <ButtonStyled.Icon>
+          <Image source={leftIcon} style={imageStyles.icon} />
+        </ButtonStyled.Icon>
+      )}
+      <ButtonStyled.Text>{title}</ButtonStyled.Text>
+      {rightIcon && (
+        <ButtonStyled.Icon>
+          <Image source={rightIcon} style={imageStyles.icon} />
+        </ButtonStyled.Icon>
+      )}
+    </ButtonStyled>
   )
 }
 
--- practice-two/app/components/Button/styles.ts
+++ practice-two/app/components/Button/styles.ts
@@ -1,47 +1,100 @@
-import { StyleSheet } from 'react-native'
+import {
+  ButtonIcon,
+  GetProps,
+  SizableText,
+  Stack,
+  createStyledContext,
+  getTokens,
+  styled,
+  withStaticProperties,
+} from 'tamagui'
 
-import { COLORS, FONT_FAMILY } from '@constants'
+const ButtonContext = createStyledContext({
+  color: undefined,
+  ellipse: undefined,
+  fontFamily: undefined,
+  fontSize: undefined,
+  fontStyle: undefined,
+  fontWeight: undefined,
+  letterSpacing: undefined,
+  maxFontSizeMultiplier: undefined,
+  size: undefined,
+  textAlign: undefined,
+})
 
-const styles = StyleSheet.create({
-  btn: {
-    paddingVertical: 13,
-    borderRadius: 50,
-    borderWidth: 2,
-    borderColor: COLORS.PRIMARY,
+const CustomButtonFrame = styled(Stack, {
+  name: 'Button',
+  context: ButtonContext,
+  paddingVertical: '$space.3',
+  borderRadius: '$radius.12',
+  borderWidth: '$space.1',
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'center',
+  alignItems: 'center',
+  columnGap: getTokens().space[2].val - 1,
+  pressStyle: {
+    opacity: 0.5,
   },
-  btnSmall: {
-    alignSelf: 'flex-start',
-    paddingVertical: 7,
-    paddingHorizontal: 20,
-  },
-  primary: {
-    backgroundColor: COLORS.PRIMARY,
-  },
-  secondary: {
-    backgroundColor: COLORS.SECONDARY,
-    borderColor: COLORS.TRANSPARENT,
-  },
-  tertiary: {
-    backgroundColor: COLORS.TRANSPARENT,
-    borderColor: COLORS.WHITE,
-  },
-  quaternary: {
-    borderColor: COLORS.WHITE,
-    backgroundColor: COLORS.WHITE,
-    borderRadius: 0,
-  },
-  btnContent: {
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    columnGap: 6,
-  },
-  btnTitle: {
-    fontFamily: FONT_FAMILY.MONTSERRAT[500],
-    textAlign: 'center',
-    textTransform: 'capitalize',
+
+  variants: {
+    variant: {
+      primary: {
+        borderColor: '$color.primary',
+        backgroundColor: '$color.primary',
+        color: '$color.white',
+      },
+
+      secondary: {
+        borderColor: '$color.transparent',
+        backgroundColor: '$color.secondary',
+        color: '$color.primary',
+      },
+
+      tertiary: {
+        borderColor: '$color.white',
+        backgroundColor: '$color.transparent',
+        color: '$color.white',
+      },
+
+      quaternary: {
+        borderRadius: '$radius.0',
+        borderColor: '$color.white',
+        backgroundColor: '$color.white',
+        color: '$color.black',
+      },
+    },
+
+    shrink: {
+      true: {
+        alignSelf: 'flex-start',
+        paddingVertical: '$space.2',
+        paddingHorizontal: getTokens().space[4].val + 2,
+      },
+    },
+  } as const,
+
+  defaultVariants: {
+    variant: 'primary',
   },
 })
 
-export default styles
+const ButtonText = styled(SizableText, {
+  name: 'Button',
+  context: ButtonContext,
+  userSelect: 'none',
+  size: '$2',
+  fontWeight: '$2',
+  textTransform: 'capitalize',
+  textAlign: 'center',
+})
+
+const ButtonStyled = withStaticProperties(CustomButtonFrame, {
+  Text: ButtonText,
+  Icon: ButtonIcon,
+  Props: ButtonContext.Provider,
+})
+
+export type ButtonProps = GetProps<typeof CustomButtonFrame>
+
+export default ButtonStyled
