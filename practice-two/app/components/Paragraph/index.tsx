a5e818fab33af3ba84f768955b74b829cf46deeb
Toan <92291488+QToan02@users.noreply.github.com>
2023-11-20 11:24:30 +0700

    Wrap Paragraph component with styleable for re-styled

--- practice-two/app/components/Paragraph/index.tsx
+++ practice-two/app/components/Paragraph/index.tsx
@@ -3,12 +3,16 @@ import isEqual from 'react-fast-compare'
 
 import StyledParagraph, { StyledParagraphProps } from './styles'
 
-export type ParagraphProps = {
+export type ParagraphProps = StyledParagraphProps & {
   content: string
-} & StyledParagraphProps
-
-const Paragraph = ({ content, ...rest }: ParagraphProps) => {
-  return <StyledParagraph {...rest}>{content}</StyledParagraph>
 }
+
+const Paragraph = StyledParagraph.styleable<ParagraphProps>(({ content, ...rest }, ref) => {
+  return (
+    <StyledParagraph ref={ref} lineHeight="$true" {...rest}>
+      {content}
+    </StyledParagraph>
+  )
+})
 
 export default memo(Paragraph, isEqual)
