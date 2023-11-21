dffa7e3b1fbf78132ebcc79711c5bd8a95b2ddc3
Toan <92291488+QToan02@users.noreply.github.com>
2023-11-21 13:43:41 +0700

    #14 Refactor SignUp screen

--- practice-two/app/screens/SignUp/index.tsx
+++ practice-two/app/screens/SignUp/index.tsx
@@ -1,17 +1,18 @@
 import { useCallback, useMemo } from 'react'
-import { Alert, KeyboardAvoidingView, Platform, View } from 'react-native'
+import { Alert, KeyboardAvoidingView, Platform } from 'react-native'
 import { SubmitHandler, useForm } from 'react-hook-form'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { NativeStackScreenProps } from '@react-navigation/native-stack'
-import { RootStackParamList } from '@navigation/Stack'
+import { XStack, YStack } from 'tamagui'
 
+import { RootStackParamList } from '@navigation/Stack'
 import { Button, Heading, Input, Paragraph } from '@components'
 import { IForm } from '@types'
 import { SIGN_UP_INPUTS } from '@constants'
 
 import styles from './styles'
 
-export interface SignUpScreenProps extends NativeStackScreenProps<RootStackParamList, 'SignUp'> {}
+export type SignUpScreenProps = NativeStackScreenProps<RootStackParamList, 'SignUp'>
 
 const SignUp = ({ navigation }: SignUpScreenProps) => {
   const { control, handleSubmit, watch } = useForm<IForm>()
@@ -19,7 +20,8 @@ const SignUp = ({ navigation }: SignUpScreenProps) => {
   const onSubmit: SubmitHandler<IForm> = (data) => {
     Alert.alert(JSON.stringify(data))
   }
-  const handleToLoginScreen = useCallback(() => navigation.navigate('Login'), [navigation])
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const handleToLoginScreen = useCallback(() => navigation.navigate('Login'), [])
   const SignUpInputs = useMemo(
     () =>
       SIGN_UP_INPUTS(observePassword).map(({ name, ...props }) => (
@@ -30,29 +32,33 @@ const SignUp = ({ navigation }: SignUpScreenProps) => {
 
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.titleContainer}>
-        <Heading content="Welcome to tradly" textMedium />
-        <Paragraph content="Signup to your account" size="md" />
-      </View>
+      <YStack marginBottom="$space.5" space="$space.9" alignItems="center">
+        <Heading content="Welcome to tradly" fontWeight="$2" letterSpacing="$2" />
+        <Paragraph content="Signup to your account" fontSize="$2" letterSpacing="$4" />
+      </YStack>
       <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
         {SignUpInputs}
       </KeyboardAvoidingView>
       <Button
-        style={styles.btn}
         title="create"
         variant="secondary"
+        marginVertical="$space.7"
+        letterSpacing="$4"
+        fontSize="$2"
         onPress={handleSubmit(onSubmit)}
       />
-      <View style={styles.info}>
-        <Paragraph size="lg" content="Have an account?" />
+      <XStack justifyContent="center" alignItems="baseline" space="$space.1">
+        <Paragraph content="Have an account?" fontSize="$3" lineHeight="$4" />
         <Button
-          style={styles.signInBtn}
-          titleStyle={styles.btnTitle}
           title="Sign in"
           variant="tertiary"
+          borderColor="$color.transparent"
+          fontWeight="$3"
+          fontSize="$3"
+          lineHeight="$4"
           onPress={handleToLoginScreen}
         />
-      </View>
+      </XStack>
     </SafeAreaView>
   )
 }
--- practice-two/app/screens/SignUp/styles.ts
+++ practice-two/app/screens/SignUp/styles.ts
@@ -1,6 +1,6 @@
 import { StyleSheet } from 'react-native'
 
-import { COLORS, FONT_FAMILY } from '@constants'
+import { COLORS } from '@constants'
 
 const styles = StyleSheet.create({
   container: {
@@ -8,26 +8,6 @@ const styles = StyleSheet.create({
     backgroundColor: COLORS.PRIMARY,
     paddingHorizontal: 32,
     justifyContent: 'center',
-  },
-  titleContainer: {
-    rowGap: 54,
-    alignItems: 'center',
-    marginBottom: 25,
-  },
-  btn: {
-    marginVertical: 40,
-  },
-  info: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'baseline',
-    columnGap: 3,
-  },
-  btnTitle: {
-    fontFamily: FONT_FAMILY.MONTSERRAT[600],
-  },
-  signInBtn: {
-    borderColor: COLORS.TRANSPARENT,
   },
 })
 
