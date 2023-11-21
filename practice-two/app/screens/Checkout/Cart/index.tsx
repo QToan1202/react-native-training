6265ba5b2b3dbf3c99c472b63055034b5165fdee
Toan <92291488+QToan02@users.noreply.github.com>
2023-11-21 16:16:47 +0700

    #14 Refactor Cart screen

--- practice-two/app/screens/Checkout/Cart/index.tsx
+++ practice-two/app/screens/Checkout/Cart/index.tsx
@@ -1,24 +1,24 @@
 import { useCallback } from 'react'
-import { ScrollView, View } from 'react-native'
 import { NativeStackScreenProps } from '@react-navigation/native-stack'
-import { RootStackParamList } from '@navigation/Stack'
+import { ScrollView, YStack } from 'tamagui'
 
+import { RootStackParamList } from '@navigation/Stack'
 import { Button, CartItem, Price, TabBar } from '@components'
 import { CHECKOUT } from '@constants'
 
-import styles from './styles'
-
-export interface CartScreenProps extends NativeStackScreenProps<RootStackParamList, 'Cart'> {}
+export type CartScreenProps = NativeStackScreenProps<RootStackParamList, 'Cart'>
 
 const Cart = ({ navigation }: CartScreenProps) => {
-  const handlePress = useCallback(() => navigation.navigate('AddAddress'), [navigation])
-  const handleNavigateAddCard = useCallback(() => navigation.navigate('Payment'), [navigation])
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const handlePress = useCallback(() => navigation.navigate('AddAddress'), [])
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const handleNavigateAddCard = useCallback(() => navigation.navigate('Payment'), [])
 
   return (
     <>
-      <ScrollView contentContainerStyle={styles.container}>
-        <Button variant="quaternary" title="+ add new address" onPress={handlePress} />
-        <View style={styles.itemList}>
+      <ScrollView flex={1} backgroundColor="$color.bg_layer">
+        <Button title="+ add new address" variant="quaternary" onPress={handlePress} />
+        <YStack marginTop="$space.2.5" marginBottom="$space.10">
           <CartItem
             image={require('@assets/cart/item.png')}
             name="coca cola"
@@ -27,12 +27,12 @@ const Cart = ({ navigation }: CartScreenProps) => {
             quantity={1}
           />
           <Button
-            titleStyle={styles.btnTitle}
-            variant="quaternary"
             title="remove"
+            variant="quaternary"
+            color="$color.gray_50"
             onPress={handlePress}
           />
-        </View>
+        </YStack>
         <Price data={CHECKOUT.PRICE_DETAILS} total={25} />
       </ScrollView>
       <TabBar title="Continue to Payment" onPress={handleNavigateAddCard} />
