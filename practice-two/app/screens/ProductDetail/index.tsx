0215ae46d44e2f988435443c92e0c3f01cee89ef
Toan <92291488+QToan02@users.noreply.github.com>
2023-11-22 11:35:17 +0700

    #14 Refactor Product Detail screen

--- practice-two/app/screens/ProductDetail/index.tsx
+++ practice-two/app/screens/ProductDetail/index.tsx
@@ -1,95 +1,176 @@
 import React, { useCallback, useMemo } from 'react'
-import { ImageBackground, ScrollView, View } from 'react-native'
+import { ScrollView, XStack, YStack } from 'tamagui'
 import { NativeStackScreenProps } from '@react-navigation/native-stack'
-import { RootStackParamList } from '@navigation/Stack'
 
 import { Avatar, Button, Heading, IconButton, Paragraph, TabBar } from '@components'
-import { containerStyles } from '@styles'
 import { CATEGORY } from '@constants'
+import { RootStackParamList } from '@navigation/Stack'
 
-import styles from './styles'
+import StyledImageBackground from './styles'
 
-export interface ProductDetailProps
-  extends NativeStackScreenProps<RootStackParamList, 'ProductDetail'> {}
+export type ProductDetailProps = NativeStackScreenProps<RootStackParamList, 'ProductDetail'>
 
 const ProductDetail = ({ navigation }: ProductDetailProps) => {
-  const handleBackPress = useCallback(() => navigation.goBack(), [navigation])
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const handleBackPress = useCallback(() => navigation.goBack(), [])
   const handlePress = useCallback(() => undefined, [])
-  const handleNavigateToCart = useCallback(() => navigation.navigate('Cart'), [navigation])
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const handleNavigateToCart = useCallback(() => navigation.navigate('Cart'), [])
   const Categories: React.JSX.Element[] = useMemo(
     () =>
       CATEGORY.map((item: string) => (
-        <Paragraph key={item} style={[styles.text, styles.textTransform]} content={item} />
+        <Paragraph
+          key={item}
+          content={item}
+          color="$color.gray_50"
+          lineHeight="$4"
+          textTransform="capitalize"
+        />
       )),
     []
   )
 
   return (
     <>
-      <ScrollView contentContainerStyle={styles.container} showsVerticalScrollIndicator={false}>
-        <View>
-          <ImageBackground
-            style={[containerStyles.inline, containerStyles.spaceBetween, styles.img]}
-            source={require('@assets/cart/item.png')}
-          >
+      <ScrollView
+        backgroundColor="$color.bg_layer"
+        space="$space.2"
+        showsVerticalScrollIndicator={false}
+      >
+        <YStack>
+          <StyledImageBackground source={require('@assets/cart/item.png')}>
             <IconButton icon={require('@assets/back.png')} onPress={handleBackPress} />
-            <View style={[containerStyles.inline]}>
+            <XStack alignItems="center" space="$space.1.5">
               <IconButton icon={require('@assets/share.png')} onPress={handlePress} />
               <IconButton icon={require('@assets/heart.png')} onPress={handlePress} />
               <IconButton icon={require('@assets/more.png')} onPress={handlePress} />
-            </View>
-          </ImageBackground>
-          <View style={styles.titleWrapper}>
-            <Heading style={styles.title} content="Coca Cola" />
-            <Heading style={[styles.title, styles.price]} content="$25" />
-          </View>
-        </View>
-        <View style={[containerStyles.inline, containerStyles.spaceBetween, styles.store]}>
+            </XStack>
+          </StyledImageBackground>
+          <YStack
+            space="$space.2.5"
+            backgroundColor="$color.white"
+            paddingHorizontal="$space.3.5"
+            paddingTop="$space.3.5"
+            paddingBottom="$space.5"
+          >
+            <Heading content="Coca Cola" fontSize="$3" color="$color.gray_50" />
+            <Heading content="$25" fontSize="$3" color="$color.primary" />
+          </YStack>
+        </YStack>
+        <XStack
+          alignItems="center"
+          justifyContent="space-between"
+          paddingVertical="$space.4.5"
+          paddingHorizontal="$space.3"
+          backgroundColor="$color.white"
+        >
           <Avatar size="md" source={require('@assets/avatar.png')} name="tradly store" />
           <Button
-            style={styles.btn}
-            titleStyle={styles.btnTitle}
-            title="follow"
             shrink
+            title="follow"
+            paddingVertical="$space.1.5"
+            paddingHorizontal="$space.5"
+            fontSize={12}
             onPress={handlePress}
           />
-        </View>
-        <View style={styles.contentWrapper}>
+        </XStack>
+        <YStack
+          paddingBottom="$space.3"
+          paddingHorizontal="$space.6"
+          borderRadius="$radius.4"
+          space="$space.3.5"
+          backgroundColor="$color.white"
+        >
           <Paragraph
             content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lobortis cras placerat diam ipsum ut. Nisi vel adipiscing massa bibendum diam. Suspendisse mattis dui maecenas duis mattis. Mattis aliquam at arcu, semper nunc, venenatis et pellentesque eu. Id tristique maecenas tristique habitasse eu elementum sed. Aliquam eget lacus, arcu, adipiscing eget feugiat in dolor sagittis.
 Non commodo, a justo massa porttitor sed placerat in. Orci tristique etiam tempus sed. Mi varius morbi egestas dictum tempor nisl. In "
-            style={styles.text}
+            color="$color.gray_50"
+            lineHeight="$4"
           />
-        </View>
-        <View style={styles.contentWrapper}>
-          <Heading style={[styles.text, styles.contentTitle]} content="Details" />
-          <View style={[containerStyles.inline, styles.detailWrapper]}>
-            <View style={[styles.category, styles.spacing]}>{Categories}</View>
-            <View style={[styles.categoryInfo, styles.spacing]}>
-              <Paragraph style={[styles.text, styles.textTransform]} content="organic" />
-              <Paragraph style={[styles.text, styles.textTransform]} content="fixed" />
-              <Paragraph style={[styles.text, styles.textTransform]} content="beverages" />
+        </YStack>
+        <YStack
+          paddingBottom="$space.3"
+          paddingHorizontal="$space.6"
+          borderRadius="$radius.4"
+          space="$space.3.5"
+          backgroundColor="$color.white"
+        >
+          <Heading
+            content="Details"
+            paddingVertical="$space.3"
+            color="$color.black"
+            fontSize="$3"
+            fontWeight="$3"
+            lineHeight="$4"
+            textTransform="capitalize"
+          />
+          <XStack alignItems="center" space="$space.6">
+            <YStack maxWidth={120} space="$space.3.5" opacity={0.7}>
+              {Categories}
+            </YStack>
+            <YStack maxWidth={200} space="$space.3.5">
               <Paragraph
-                style={[styles.text, styles.textTransform]}
+                content="organic"
+                color="$color.gray_50"
+                lineHeight="$4"
+                textTransform="capitalize"
+              />
+              <Paragraph
+                content="fixed"
+                color="$color.gray_50"
+                lineHeight="$4"
+                textTransform="capitalize"
+              />
+              <Paragraph
+                content="beverages"
+                color="$color.gray_50"
+                lineHeight="$4"
+                textTransform="capitalize"
+              />
+              <Paragraph
                 content="kualalumpur, malaysia"
+                color="$color.gray_50"
+                lineHeight="$4"
+                textTransform="capitalize"
               />
-            </View>
-          </View>
-        </View>
-        <View style={styles.contentWrapper}>
-          <Heading style={[styles.text, styles.contentTitle]} content="Additional Details" />
-          <View style={[containerStyles.inline, styles.detailWrapper]}>
-            <View style={[styles.category, styles.spacing]}>
-              <Paragraph style={[styles.text, styles.textTransform]} content="delivery details" />
-            </View>
-            <View style={[styles.categoryInfo, styles.spacing]}>
+            </YStack>
+          </XStack>
+        </YStack>
+        <YStack
+          paddingBottom="$space.3"
+          paddingHorizontal="$space.6"
+          borderRadius="$radius.4"
+          space="$space.3.5"
+          backgroundColor="$color.white"
+        >
+          <Heading
+            content="Additional Details"
+            paddingVertical="$space.3"
+            color="$color.black"
+            fontSize="$3"
+            fontWeight="$3"
+            lineHeight="$4"
+            textTransform="capitalize"
+          />
+          <XStack alignItems="center" space="$space.6">
+            <YStack maxWidth={120} space="$space.3.5" opacity={0.7}>
               <Paragraph
-                style={[styles.text, styles.textTransform]}
-                content="Home Delivery Available, Cash On Delivery"
+                content="delivery details"
+                color="$color.gray_50"
+                lineHeight="$4"
+                textTransform="capitalize"
               />
-            </View>
-          </View>
-        </View>
+            </YStack>
+            <YStack maxWidth={200} space="$space.3.5">
+              <Paragraph
+                content="Home Delivery Available, Cash On Delivery"
+                color="$color.gray_50"
+                lineHeight="$4"
+                textTransform="capitalize"
+              />
+            </YStack>
+          </XStack>
+        </YStack>
       </ScrollView>
       <TabBar title="Add To Cart" onPress={handleNavigateToCart} />
     </>
--- practice-two/app/screens/ProductDetail/styles.ts
+++ practice-two/app/screens/ProductDetail/styles.ts
@@ -1,77 +1,16 @@
-import { StyleSheet } from 'react-native'
-import { COLORS, FONT_FAMILY } from '@constants'
+import { ImageBackground } from 'react-native'
+import { styled } from 'tamagui'
 
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: COLORS.BG_LAYER,
-    rowGap: 6,
-  },
-  img: {
-    paddingTop: 16,
-    paddingHorizontal: 12,
-    paddingBottom: 180,
-  },
-  titleWrapper: {
-    paddingTop: 16,
-    paddingBottom: 25,
-    paddingHorizontal: 16,
-    backgroundColor: COLORS.WHITE,
-    rowGap: 10,
-  },
-  title: {
-    fontSize: 18,
-    color: COLORS.GRAY_50,
-  },
-  price: {
-    color: COLORS.PRIMARY,
-  },
-  store: {
-    backgroundColor: COLORS.WHITE,
-    paddingVertical: 20,
-    paddingHorizontal: 12,
-  },
-  btn: {
-    paddingHorizontal: 23,
-    paddingVertical: 4,
-  },
-  btnTitle: {
-    fontSize: 12,
-  },
-  text: {
-    color: COLORS.GRAY_50,
-    lineHeight: 20,
-  },
-  contentWrapper: {
-    paddingBottom: 13,
-    paddingHorizontal: 30,
-    borderRadius: 8,
-    rowGap: 15,
-    backgroundColor: COLORS.WHITE,
-  },
-  contentTitle: {
-    paddingVertical: 14,
-    fontSize: 18,
-    fontFamily: FONT_FAMILY.MONTSERRAT[600],
-    color: COLORS.BLACK,
-    textTransform: 'capitalize',
-  },
-  category: {
-    opacity: 0.7,
-    minWidth: 120,
-  },
-  detailWrapper: {
-    columnGap: 32,
-    alignItems: 'flex-start',
-  },
-  categoryInfo: {
-    maxWidth: 200,
-  },
-  spacing: {
-    rowGap: 15,
-  },
-  textTransform: {
-    textTransform: 'capitalize',
+const StyledImageBackground = styled(ImageBackground, {
+  source: { uri: '' },
+  paddingTop: 16,
+  paddingHorizontal: 12,
+  height: 230,
+
+  style: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
 })
 
-export default styles
+export default StyledImageBackground
