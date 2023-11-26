c1954b110475480475acea48dbd4f8f6b3fb21f5
Toan <92291488+QToan02@users.noreply.github.com>
2023-11-26 20:37:32 +0700

    #15 Handle login user and save data in AsyncStorage

--- practice-two/app/screens/Login/index.tsx
+++ practice-two/app/screens/Login/index.tsx
@@ -1,26 +1,44 @@
-import { useCallback } from 'react'
-import { Alert, KeyboardAvoidingView, Platform } from 'react-native'
+import { useCallback, useEffect } from 'react'
+import { KeyboardAvoidingView, Platform } from 'react-native'
 import { XStack, YStack } from 'tamagui'
 import { SubmitHandler, useForm } from 'react-hook-form'
 import { SafeAreaView } from 'react-native-safe-area-context'
+
 import { NativeStackScreenProps } from '@react-navigation/native-stack'
 import { RootStackParamList } from '@navigation/Stack'
-
 import { Button, Heading, Input, Paragraph } from '@components'
 import { IForm } from '@types'
 import { COLORS, ERROR_MESSAGES } from '@constants'
+import { useLogin } from '@hooks'
+import { asyncStoreService } from '@services'
+import { useAuthStore } from '@stores'
 
 import styles from './styles'
 
 export interface LoginScreenProps extends NativeStackScreenProps<RootStackParamList, 'Login'> {}
 
 const Login = ({ navigation }: LoginScreenProps) => {
+  const { mutate, data: user, isSuccess } = useLogin(process.env.EXPO_PUBLIC_API_URL)
+  const loginFn = useAuthStore((state) => state.login)
   const { control, handleSubmit } = useForm<IForm>()
-  const onSubmit: SubmitHandler<IForm> = (data) => {
-    Alert.alert(JSON.stringify(data))
-  }
-  const handleToSignUpScreen = useCallback(() => navigation.navigate('SignUp'), [navigation])
+  const onSubmit: SubmitHandler<IForm> = useCallback(({ email, password }: IForm) => {
+    mutate({
+      email,
+      password,
+    })
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+  const handleSaveUser = useCallback(async () => {
+    if (!isSuccess) return
 
+    await asyncStoreService.save('user', user, () => loginFn(user))
+  }, [isSuccess, loginFn, user])
+
+  useEffect(() => {
+    handleSaveUser()
+  }, [handleSaveUser])
+
+  const handleToSignUpScreen = useCallback(() => navigation.navigate('SignUp'), [navigation])
   return (
     <SafeAreaView style={styles.container}>
       <YStack space="$space.9" alignItems="center">
