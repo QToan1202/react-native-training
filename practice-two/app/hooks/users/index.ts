f09949edeca4c735f1145a5119e5dbc33332977b
Toan <92291488+QToan02@users.noreply.github.com>
2023-11-26 20:36:37 +0700

    Optimal useLogin hook

--- practice-two/app/hooks/users/index.ts
+++ practice-two/app/hooks/users/index.ts
@@ -6,12 +6,10 @@ import { IUser } from '@types'
 const useLogin = (
   path: string
 ): UseMutationResult<IUser, Error, Pick<IUser, 'email' | 'password'>, unknown> => {
-  const loginMutate = useMutation<IUser, Error, Pick<IUser, 'email' | 'password'>, unknown>({
+  return useMutation<IUser, Error, Pick<IUser, 'email' | 'password'>, unknown>({
     mutationFn: ({ email, password }: Pick<IUser, 'email' | 'password'>): Promise<IUser> =>
       login(path, email, password),
   })
-
-  return loginMutate
 }
 
 export default useLogin
