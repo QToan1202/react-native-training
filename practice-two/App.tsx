6379f682cbadb1c8337233ef212ae7945cc179dd
Toan <92291488+QToan02@users.noreply.github.com>
2023-11-30 10:55:29 +0700

    Setup check hydrated storage and checking login user

--- practice-two/App.tsx
+++ practice-two/App.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useRef, useState } from 'react'
+import { useCallback } from 'react'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 import * as SplashScreen from 'expo-splash-screen'
 import {
@@ -8,40 +8,23 @@ import {
   Montserrat_600SemiBold,
   Montserrat_700Bold,
 } from '@expo-google-fonts/montserrat'
-import { StatusBar, StatusBarStyle } from 'expo-status-bar'
 import { TamaguiProvider } from 'tamagui'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { useShallow } from 'zustand/react/shallow'
 
 import { NavigationContainer } from '@react-navigation/native'
 import { BottomNav, StackNavigation } from '@navigation'
-import { asyncStoreService } from '@services'
+import { useAuthStore } from '@stores'
 
 import styles from './App.styles'
 import StorybookUI from './.storybook'
 import config from './tamagui.config'
 
 SplashScreen.preventAutoHideAsync()
-const FAKE_IS_LOGIN = true
 const queryClient = new QueryClient()
 
 const App = () => {
-  const theme = useRef<StatusBarStyle>('auto')
-  const [isCompleteLoadTheme, setIsCompleteLoadTheme] = useState(false)
-  const loadTheme = useCallback(async (): Promise<StatusBarStyle> => {
-    try {
-      const storedTheme: string = await asyncStoreService.get<string>('appTheme')
-      const isStatusBarStyle = (value: string): value is StatusBarStyle =>
-        ['auto', 'inverted', 'light', 'dark'].includes(value)
-
-      if (!isStatusBarStyle(storedTheme)) return 'light'
-
-      return storedTheme
-    } catch (error) {
-      return 'light'
-    } finally {
-      setIsCompleteLoadTheme(true)
-    }
-  }, [])
+  const [isHydrated, user] = useAuthStore(useShallow((state) => [state.isHydrated, state.user]))
 
   const [fontsLoaded] = useFonts({
     Montserrat_400Regular,
@@ -51,11 +34,10 @@ const App = () => {
   })
 
   const onLayoutRootView = useCallback(async () => {
-    theme.current = await loadTheme()
-    if (fontsLoaded && isCompleteLoadTheme) {
+    if (fontsLoaded && isHydrated) {
       await SplashScreen.hideAsync()
     }
-  }, [fontsLoaded, isCompleteLoadTheme, loadTheme])
+  }, [fontsLoaded, isHydrated])
 
   if (!fontsLoaded) return null
 
@@ -63,9 +45,8 @@ const App = () => {
     <QueryClientProvider client={queryClient}>
       <TamaguiProvider config={config}>
         <SafeAreaProvider style={styles.container} onLayout={onLayoutRootView}>
-          <StatusBar style={theme.current} />
           <NavigationContainer>
-            {!FAKE_IS_LOGIN ? <StackNavigation.PublicStackNavigator /> : <BottomNav />}
+            {!user ? <StackNavigation.PublicStackNavigator /> : <BottomNav />}
           </NavigationContainer>
         </SafeAreaProvider>
       </TamaguiProvider>
--- practice-two/app/stores/authentication/index.ts
+++ practice-two/app/stores/authentication/index.ts
@@ -5,6 +5,8 @@ import { createJSONStorage, persist } from 'zustand/middleware'
 import { IUser } from '@types'
 
 interface AuthState {
+  isHydrated: boolean
+  setIsHydrated: (isHydratedState: boolean) => void
   user: IUser | undefined
   login: (user: IUser) => void
   logout: () => void
@@ -13,15 +15,19 @@ interface AuthState {
 export const useAuthStore = create<AuthState>()(
   persist(
     (set) => ({
+      isHydrated: false,
+      setIsHydrated: (isHydratedState: boolean) => set({ isHydrated: isHydratedState }),
+
       user: undefined,
-
       login: (user: IUser) => set(() => ({ user })),
-
       logout: () => set(() => ({ user: undefined })),
     }),
     {
       name: 'user.storage',
       storage: createJSONStorage(() => AsyncStorage),
+      onRehydrateStorage: () => (state: AuthState | undefined) => {
+        state?.setIsHydrated(true)
+      },
     }
   )
 )
