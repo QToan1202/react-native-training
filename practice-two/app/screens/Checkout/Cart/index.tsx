f67b3954ed1a392d46420c675078eb93b1497ed1
Toan <92291488+QToan02@users.noreply.github.com>
2023-12-04 09:42:33 +0700

    Show address component when current address is available

--- practice-two/app/screens/Checkout/Cart/index.tsx
+++ practice-two/app/screens/Checkout/Cart/index.tsx
@@ -1,23 +1,42 @@
-import { useCallback } from 'react'
+import { useCallback, useMemo } from 'react'
 import { NativeStackScreenProps } from '@react-navigation/native-stack'
 import { ScrollView, YStack } from 'tamagui'
+import { useShallow } from 'zustand/react/shallow'
 
 import { RootStackParamList } from '@navigation/Stack'
-import { Button, CartItem, Price, TabBar } from '@components'
+import { Address, Button, CartItem, Price, TabBar } from '@components'
 import { CHECKOUT } from '@constants'
+import { useOrderStore } from '@stores'
 
 export type CartScreenProps = NativeStackScreenProps<RootStackParamList, 'Cart'>
 
 const Cart = ({ navigation }: CartScreenProps) => {
+  const [isHydrated, address] = useOrderStore(
+    useShallow((state) => [state.isHydrated, state.address])
+  )
   // eslint-disable-next-line react-hooks/exhaustive-deps
-  const handlePress = useCallback(() => navigation.navigate('AddAddress'), [])
+  const handleChangeAddress = useCallback(() => navigation.navigate('AddAddress'), [])
   // eslint-disable-next-line react-hooks/exhaustive-deps
   const handleNavigateAddCard = useCallback(() => navigation.navigate('Payment'), [])
+  const renderAddress = useMemo(() => {
+    if (!isHydrated) return null
+    if (!address) return null
+
+    return (
+      <Address
+        name={`Deliver to ${address.name}`}
+        streetAddress={address.streetAddress}
+        onPress={handleChangeAddress}
+      />
+    )
+  }, [isHydrated, address, handleChangeAddress])
 
   return (
     <>
       <ScrollView flex={1} backgroundColor="$color.bg_layer">
-        <Button title="+ add new address" variant="quaternary" onPress={handlePress} />
+        {renderAddress || (
+          <Button title="+ add new address" variant="quaternary" onPress={handleChangeAddress} />
+        )}
         <YStack marginTop="$space.2.5" marginBottom="$space.10">
           <CartItem
             image={require('@assets/cart/item.png')}
@@ -30,7 +49,7 @@ const Cart = ({ navigation }: CartScreenProps) => {
             title="remove"
             variant="quaternary"
             color="$color.gray_50"
-            onPress={handlePress}
+            onPress={handleChangeAddress}
           />
         </YStack>
         <Price data={CHECKOUT.PRICE_DETAILS} total={25} />
