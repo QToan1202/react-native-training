517e23744d529946892142a025c4c20661d20ab5
Toan <92291488+QToan02@users.noreply.github.com>
2024-01-18 08:50:31 +0700

    Refactor render Radio component by 'map' method

--- practice-two/app/components/Radio/index.tsx
+++ practice-two/app/components/Radio/index.tsx
@@ -1,14 +1,6 @@
 import { memo, useCallback, useMemo, useState } from 'react'
 import isEqual from 'react-fast-compare'
-import { FlatList, ListRenderItem } from 'react-native'
-import {
-  Label,
-  RadioGroup,
-  RadioGroupProps,
-  Separator as TSeparator,
-  XStack,
-  YStack,
-} from 'tamagui'
+import { Label, RadioGroup, RadioGroupProps, Separator, XStack, YStack } from 'tamagui'
 
 import { IRadioItem } from '@types'
 
@@ -16,47 +8,46 @@ import styles, { StyledIndicator, StyledItem } from './styles'
 
 export type RadioProps = RadioGroupProps & {
   radioList: IRadioItem[]
+  onChangeValue?: (value: string) => void
 }
 
-const Separator = () => <TSeparator alignSelf="stretch" borderColor="$color.divider" />
-
-const Radio = ({ radioList, ...rest }: RadioProps) => {
+const Radio = ({ radioList, onChangeValue, ...rest }: RadioProps) => {
   const [selectedItem, setSelectedItem] = useState<string>()
-  const handleSelectedRadio = useCallback((value: string): void => setSelectedItem(value), [])
-  const renderItem: ListRenderItem<IRadioItem> = useCallback(
-    ({ item: { id, name } }) => (
-      <XStack
-        alignItems="center"
-        space="$space.2"
-        paddingLeft="$space.3.5"
-        paddingVertical="$space.3"
-      >
-        <StyledItem
-          value={name}
-          id={id}
-          style={selectedItem === name ? styles.selected : styles.normal}
+  const handleSelectedRadio = useCallback(
+    (value: string): void => {
+      if (onChangeValue) onChangeValue(value)
+      setSelectedItem(value)
+    },
+    [onChangeValue]
+  )
+  const renderList = useMemo(() => {
+    return radioList.map(({ id, name }: IRadioItem, index: number) => (
+      <YStack key={id}>
+        <XStack
+          alignItems="center"
+          space="$space.2"
+          paddingLeft="$space.3.5"
+          paddingVertical="$space.3"
         >
-          <StyledIndicator />
-        </StyledItem>
+          <StyledItem
+            value={name}
+            id={id}
+            style={selectedItem === name ? styles.selected : styles.normal}
+          >
+            <StyledIndicator />
+          </StyledItem>
 
-        <Label unstyled htmlFor={id} fontWeight="$3" color="$color.gray_50" lineHeight="$4">
-          {name}
-        </Label>
-      </XStack>
-    ),
-    [selectedItem]
-  )
-  const renderList = useMemo(
-    () => (
-      <FlatList
-        data={radioList}
-        keyExtractor={({ id }: IRadioItem): string => id}
-        renderItem={renderItem}
-        ItemSeparatorComponent={Separator}
-      />
-    ),
-    [radioList, renderItem]
-  )
+          <Label unstyled htmlFor={id} fontWeight="$3" color="$color.gray_50" lineHeight="$4">
+            {name}
+          </Label>
+        </XStack>
+        {/* Do not render Separator component on the last item */}
+        {index !== radioList.length - 1 && (
+          <Separator alignSelf="stretch" borderColor="$color.divider" />
+        )}
+      </YStack>
+    ))
+  }, [radioList, selectedItem])
 
   return (
     <RadioGroup onValueChange={handleSelectedRadio} {...rest}>
