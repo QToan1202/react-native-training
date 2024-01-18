64f26cee95ac8e096f3b5610f1a2caf1a2930647
Toan <92291488+QToan02@users.noreply.github.com>
2024-01-18 10:02:11 +0700

    Create product common styles

--- practice-two/app/styles/product/index.ts
+++ practice-two/app/styles/product/index.ts
@@ -6,6 +6,15 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: COLORS.WHITE,
   },
+  item: {
+    paddingVertical: 30,
+    rowGap: 10,
+    alignSelf: 'center',
+  },
+  column: {
+    columnGap: 10,
+    justifyContent: 'flex-start',
+  },
 })
 
 export default styles
