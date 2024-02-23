de5fb2a99e18a28e3363106f2dcc020d670a0b5a
Toan <92291488+QToan02@users.noreply.github.com>
2024-02-23 14:26:40 +0700

    #26 Create Store feat

---
+++ practice-two/libs/features/Store/components/StoreCard/index.tsx
@@ -0,0 +1,55 @@
+import { memo } from 'react'
+import isEqual from 'react-fast-compare'
+import { Image, ImageBackgroundProps } from 'react-native'
+import { SizeTokens, Stack, StackProps, YStack } from 'tamagui'
+
+import Avatar, { AvatarProps } from '@components/Avatar'
+import Button from '@components/Button'
+import { containerStyles } from '@styles'
+
+import styles from './styles'
+
+export type StoreCardProps = StackProps &
+  Pick<AvatarProps, 'source' | 'name'> & {
+    bgImage: ImageBackgroundProps['source']
+    btnTitle: string
+    size?: SizeTokens
+    onPressBtn?: () => void
+  }
+
+const StoreCard = ({
+  bgImage,
+  source,
+  name,
+  size = 'xl',
+  btnTitle,
+  onPressBtn,
+  ...rest
+}: StoreCardProps) => {
+  return (
+    <Stack
+      alignSelf="baseline"
+      paddingTop="$space.9"
+      paddingBottom="$space.4"
+      paddingHorizontal="$space.5"
+      w={160}
+      style={containerStyles.card}
+      {...rest}
+    >
+      <Image source={bgImage} style={styles.img} />
+      <YStack justifyContent="center" alignItems="center">
+        <Avatar source={source} name={name} size={size} block />
+        <Button
+          marginTop="$space.4"
+          paddingHorizontal="$space.5"
+          paddingVertical={4}
+          fontSize={10}
+          title={btnTitle}
+          onPress={onPressBtn}
+        />
+      </YStack>
+    </Stack>
+  )
+}
+
+export default memo(StoreCard, isEqual)
