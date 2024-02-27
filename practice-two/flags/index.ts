6574eb8196cdf9337b818517097c3f55d493bcc7
Toan <92291488+QToan02@users.noreply.github.com>
2024-02-27 11:17:46 +0700

    #31 Create feature flags config file

---
+++ practice-two/flags/index.ts
@@ -0,0 +1,15 @@
+const featureFlags = [
+  { name: 'Onboarding', active: false },
+  { name: 'Login', active: false },
+  { name: 'SignUp', active: false },
+  { name: 'Dashboard', active: false },
+  { name: 'Category', active: false },
+  { name: 'Product', active: false },
+  { name: 'Browse', active: false },
+  { name: 'Wishlist', active: false },
+  { name: 'Checkout', active: false },
+  { name: 'Notification', active: false },
+  { name: 'Navigation', active: false },
+]
+
+export default featureFlags
