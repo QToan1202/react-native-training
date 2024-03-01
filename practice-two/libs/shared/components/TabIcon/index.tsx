1d2b5f508753533612e0d8974e0c21298e906816
Toan <92291488+QToan02@users.noreply.github.com>
2024-03-01 17:06:21 +0700

    Create TabIcon component

---
+++ practice-two/libs/shared/components/TabIcon/index.tsx
@@ -0,0 +1,45 @@
+import { SvgProps } from 'react-native-svg'
+
+import HomeIcon from '@assets/tab/home.svg'
+import OrderIcon from '@assets/tab/order.svg'
+import ProfileIcon from '@assets/tab/profile.svg'
+import SearchIcon from '@assets/tab/search.svg'
+import StoreIcon from '@assets/tab/store.svg'
+
+import { imageStyles } from '@practice-two/shared'
+
+export interface IconProps extends SvgProps {
+  color: string
+}
+
+const setIconColor = (color: string): SvgProps => ({ fill: color })
+
+const Home = ({ color, style, ...rest }: IconProps) => {
+  return <HomeIcon style={[imageStyles.icon, style]} {...setIconColor(color)} {...rest} />
+}
+
+const Order = ({ color, style, ...rest }: IconProps) => {
+  return <OrderIcon style={[imageStyles.icon, style]} {...setIconColor(color)} {...rest} />
+}
+
+const Profile = ({ color, style, ...rest }: IconProps) => {
+  return <ProfileIcon style={[imageStyles.icon, style]} {...setIconColor(color)} {...rest} />
+}
+
+const Search = ({ color, style, ...rest }: IconProps) => {
+  return <SearchIcon style={[imageStyles.icon, style]} {...setIconColor(color)} {...rest} />
+}
+
+const Store = ({ color, style, ...rest }: IconProps) => {
+  return <StoreIcon style={[imageStyles.icon, style]} {...setIconColor(color)} {...rest} />
+}
+
+const TabIcon = {
+  Home,
+  Order,
+  Profile,
+  Search,
+  Store,
+}
+
+export default TabIcon
