3f302a3f58cf7fdc636883dd8dbbecedac9d5f10
Toan <92291488+QToan02@users.noreply.github.com>
2024-03-12 11:45:02 +0700

    Handle render Alert when user access unavailable feature

--- practice-two/libs/shared/hocs/feature.tsx
+++ practice-two/libs/shared/hocs/feature.tsx
@@ -1,4 +1,7 @@
-import { ReactNode } from 'react'
+import { ReactNode, useCallback } from 'react'
+import { Alert } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
+
 import { useFeatureFlags } from '../stores'
 
 interface FeatureProps {
@@ -8,9 +11,19 @@ interface FeatureProps {
 
 const Feature = ({ feat, children }: FeatureProps) => {
   const isFeatureActive = useFeatureFlags((state) => state.isFeatureActive)
+  const navigation = useNavigation()
+  const handleNavigateBack = useCallback(() => {
+    navigation.goBack()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
   if (isFeatureActive(feat)) return children
 
+  // If feature unavailable show an alert with single option
+  // that bring user back to the previous screen
+  Alert.alert('Feature on working progress', 'This feature is coming soon!!!', [
+    { text: 'OK', onPress: handleNavigateBack },
+  ])
   return null
 }
 
