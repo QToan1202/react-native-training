f61e45067fc66a11163990474777b6004f76e56c
Toan <92291488+QToan02@users.noreply.github.com>
2024-03-19 11:29:57 +0700

    Move CustomHeader to shared/utils folder

--- practice-two/libs/shared/utils/render/index.tsx
+++ practice-two/libs/shared/utils/render/index.tsx
@@ -1,12 +1,19 @@
+import { Suspense } from 'react'
+import { Spinner } from 'tamagui'
 import { ListRenderItemInfo } from 'react-native'
+import { NativeStackHeaderProps } from '@react-navigation/native-stack'
 
 import { IFlatListBase } from '../../types'
 
-const renderItem =
+export const renderItem =
   <T extends IFlatListBase>(Element: React.JSX.ElementType, action?: (data: T) => void) =>
   // eslint-disable-next-line react/function-component-definition
   ({ item: { ...rest } }: ListRenderItemInfo<T>) => (
     <Element {...rest} onPress={() => action && action({ ...rest })} />
   )
 
-export default renderItem
+export const CustomHeader = (Element: React.JSX.ElementType, props: NativeStackHeaderProps) => (
+  <Suspense fallback={<Spinner size="large" color="$color.primary" />}>
+    <Element {...props} />
+  </Suspense>
+)
