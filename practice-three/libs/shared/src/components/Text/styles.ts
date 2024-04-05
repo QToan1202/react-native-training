9e656e632bcd68adc8353c75cbcbd60bbfb6fb2e
Toan <92291488+QToan02@users.noreply.github.com>
2024-04-05 15:59:14 +0700

    #42 Config a tag for Text component

--- practice-three/libs/shared/src/components/Text/styles.ts
+++ practice-three/libs/shared/src/components/Text/styles.ts
@@ -1,6 +1,8 @@
 import { GetProps, Text, styled } from 'tamagui'
 
 const StyledText = styled(Text, {
+  name: 'Text',
+  tag: 'p',
   fontWeight: '400',
   fontFamily: '$body',
   color: '$color.black',
