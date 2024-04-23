5e5335faace107a807927aa2badc1f01fe50f000
Toan <92291488+QToan02@users.noreply.github.com>
2024-04-23 11:52:32 +0700

    #44 Add more social login methods in Login screen

--- practice-three/libs/authentication/src/screens/Login/index.tsx
+++ practice-three/libs/authentication/src/screens/Login/index.tsx
@@ -1,4 +1,4 @@
-import { H2, Heading, Image, Separator, View, XStack, YStack } from 'tamagui'
+import { H2, Separator, XStack, YStack } from 'tamagui'
 import { SubmitHandler } from 'react-hook-form'
 
 import { Button, Checkbox, Form, Input, Text } from '@shared/components'
@@ -14,51 +14,70 @@ const Login = () => {
   }
 
   return (
-    <View>
-      <YStack gap={5} paddingHorizontal={36}>
-        <YStack alignItems="center" gap={10}>
-          <H2 color="$primary">Welcome back to E&#45;com&#33;</H2>
-          <Text color="$gray_100">Sign in to continue</Text>
-        </YStack>
-
-        <Form onSubmit={handleOnSubmit} gap={10}>
-          <Input label="email" placeholder="Your Email / Phone Number" />
-          <Input label="password" placeholder="Password" secureTextEntry />
-
-          <YStack>
-            <Text color="$primary" fontWeight="bold" fontSize="$1" textAlign="right">
-              Forgot Password&#63;
-            </Text>
-            <Checkbox label="Remember Me" />
-          </YStack>
-          <Form.Trigger asChild="web">
-            <Button title="login" borderRadius={5} />
-          </Form.Trigger>
-        </Form>
-
-        <Separator
-          position="relative"
-          marginVertical={15}
-          alignItems="center"
-          borderColor="$border"
-        >
-          <Text
-            position="absolute"
-            top={-8}
-            backgroundColor="$pure_white"
-            paddingHorizontal={20}
-            textTransform="uppercase"
-            color="$gray_100"
-            fontSize="$2"
-            fontWeight="bold"
-          >
-            Or
-          </Text>
-        </Separator>
-
-        <Text>Login using</Text>
+    <YStack gap={5} paddingHorizontal={36}>
+      <YStack alignItems="center" gap={10}>
+        <H2 color="$primary">Welcome back to E&#45;com&#33;</H2>
+        <Text color="$gray_100">Sign in to continue</Text>
       </YStack>
-    </View>
+
+      <Form onSubmit={handleOnSubmit} gap={10}>
+        <Input label="email" placeholder="Your Email / Phone Number" />
+        <Input label="password" placeholder="Password" secureTextEntry />
+
+        <YStack>
+          <Text color="$primary" fontWeight="bold" fontSize="$1" textAlign="right">
+            Forgot Password&#63;
+          </Text>
+          <Checkbox label="Remember Me" />
+        </YStack>
+        <Form.Trigger asChild="web">
+          <Button title="login" borderRadius={5} />
+        </Form.Trigger>
+      </Form>
+
+      <Separator position="relative" marginVertical={15} alignItems="center" borderColor="$border">
+        <Text
+          position="absolute"
+          top={-8}
+          backgroundColor="$pure_white"
+          paddingHorizontal={20}
+          textTransform="uppercase"
+          color="$gray_100"
+          fontSize="$2"
+          fontWeight="bold"
+        >
+          Or
+        </Text>
+      </Separator>
+
+      <YStack marginTop="$5" alignItems="center" gap="$5">
+        <Text fontSize={15}>Login using</Text>
+
+        <XStack gap="$6">
+          <img alt="ap" src={Apple} />
+          <img alt="fb" src={Facebook} />
+          <img alt="gg" src={Google} />
+        </XStack>
+
+        <YStack marginTop="$7">
+          <Text color="$gray_100" fontSize="$1" letterSpacing={0.5}>
+            Don&#39;t have an account&#63;{' '}
+            <Text
+              color="$primary"
+              hoverStyle={{
+                cursor: 'pointer',
+                textDecorationStyle: 'solid',
+                textDecorationLine: 'underline',
+              }}
+              $platform-web={{ fontWeight: 'bold' }}
+              $platform-native={{ fontWeight: '700' }}
+            >
+              Register
+            </Text>
+          </Text>
+        </YStack>
+      </YStack>
+    </YStack>
   )
 }
 
