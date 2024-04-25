2e0a1d6cc61107a3a018200b53e34c185870532e
Toan <92291488+QToan02@users.noreply.github.com>
2024-04-25 15:48:48 +0700

    Wrap provider for storybook stories

--- practice-three/libs/authentication/.storybook/preview.tsx
+++ practice-three/libs/authentication/.storybook/preview.tsx
@@ -1,14 +1,25 @@
 import React from 'react'
 import { Preview } from '@storybook/react'
 import { TamaguiProvider } from 'tamagui'
+import { ToastProvider, ToastViewport } from '@tamagui/toast'
 
+import { Toast } from '@shared/components'
 import tamaguiConfig from '../src/tamagui.config'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 const preview: Preview = {
   decorators: [
     (Story) => (
       <TamaguiProvider config={tamaguiConfig}>
-        <Story />
+        <QueryClientProvider client={queryClient}>
+          <ToastProvider>
+            <ToastViewport flexDirection="column" bottom={50} left={0} right={0} />
+            <Toast />
+            <Story />
+          </ToastProvider>
+        </QueryClientProvider>
       </TamaguiProvider>
     ),
   ],
