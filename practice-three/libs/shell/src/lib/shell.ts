649ebaef461a7ece1a2d13ca27a1b0ebc891f5ba
Toan <92291488+QToan02@users.noreply.github.com>
2024-05-06 12:20:51 +0700

    #44 Optimize feature check active state

--- practice-three/libs/shell/src/lib/shell.ts
+++ practice-three/libs/shell/src/lib/shell.ts
@@ -9,27 +9,17 @@ const preDefinedFeatures: Array<Omit<TFeatureConfig, 'active'>> = [
 ]
 
 const features = (featureList: string | ReadonlyArray<string>): Array<TFeatureConfig> => {
-  let feats: Array<TFeatureConfig> = []
   const convertFeatureList =
     typeof featureList === 'object'
       ? featureList
       : featureList.split(',').map((item: string) => item.trim())
 
-  convertFeatureList.forEach((feat: string) => {
-    const matchedFeature: Omit<TFeatureConfig, 'active'> | undefined = preDefinedFeatures.find(
-      (config) => config.name.localeCompare(feat, undefined, { sensitivity: 'base' })
-    )
-    feats = preDefinedFeatures.map((featConfig) => {
-      if (!matchedFeature) {
-        return { ...featConfig, ...{ active: false } }
-      }
-
-      return { ...featConfig, ...{ active: true } }
-    })
-  })
-  console.log(convertFeatureList, feats)
-
-  return feats
+  return preDefinedFeatures.map((config) => ({
+    ...config,
+    active: convertFeatureList.some(
+      (feat) => !feat.localeCompare(config.name, undefined, { sensitivity: 'base' })
+    ),
+  }))
 }
 
 export default features
