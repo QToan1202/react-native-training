64ffb23b8df269842ef936e2a5707e868e779e3d
Toan <92291488+QToan02@users.noreply.github.com>
2024-05-06 12:05:44 +0700

    #44 Refactor feature init shell

--- practice-three/libs/shared/src/types/hoc/index.ts
+++ practice-three/libs/shared/src/types/hoc/index.ts
@@ -1,3 +1,10 @@
 export type THOCsProps = {
+  category: string[]
   navigatorData: unknown[]
+}
+
+export type TFeatureConfig = {
+  name: string
+  description: string
+  active: boolean
 }
--- practice-three/libs/shell/src/lib/shell.ts
+++ practice-three/libs/shell/src/lib/shell.ts
@@ -1,10 +1,35 @@
-const features = [
+import { TFeatureConfig } from '@shared/types'
+
+const preDefinedFeatures: Array<Omit<TFeatureConfig, 'active'>> = [
   {
     name: 'authentication',
-    active: true,
     description:
       'Enjoy a secure and reliable authentication experience with advanced encryption and multi-factor authentication options.',
   },
 ]
+
+const features = (featureList: string | ReadonlyArray<string>): Array<TFeatureConfig> => {
+  let feats: Array<TFeatureConfig> = []
+  const convertFeatureList =
+    typeof featureList === 'object'
+      ? featureList
+      : featureList.split(',').map((item: string) => item.trim())
+
+  convertFeatureList.forEach((feat: string) => {
+    const matchedFeature: Omit<TFeatureConfig, 'active'> | undefined = preDefinedFeatures.find(
+      (config) => config.name.localeCompare(feat, undefined, { sensitivity: 'base' })
+    )
+    feats = preDefinedFeatures.map((featConfig) => {
+      if (!matchedFeature) {
+        return { ...featConfig, ...{ active: false } }
+      }
+
+      return { ...featConfig, ...{ active: true } }
+    })
+  })
+  console.log(convertFeatureList, feats)
+
+  return feats
+}
 
 export default features
