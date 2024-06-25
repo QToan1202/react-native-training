818b6673bb37f3eb4502c54b392c5730c063e527
Toan <92291488+QToan02@users.noreply.github.com>
2024-06-25 16:56:51 +0700

    Refactor passing logic `navigatorData` prop in withAuth HOC

--- practice-three/libs/authentication/src/hocs/index.tsx
+++ practice-three/libs/authentication/src/hocs/index.tsx
@@ -9,14 +9,17 @@ const FEATURE_NAME = 'authentication'
 export const withAuth = <T extends THOCsProps>(Wrapper: ComponentType<T>) => {
   return forwardRef<unknown, T>((props, componentRef) => {
     const { category, navigatorData, ...rest } = props
-    let mergeNavigator = navigatorData
     const isFeatureActive = category.find(
       (feat) => !feat.localeCompare(FEATURE_NAME, undefined, { sensitivity: 'base' })
     )
 
-    if (isFeatureActive) mergeNavigator = [...navigatorData, AuthenticationRoute]
-
-    return <Wrapper ref={componentRef} {...(rest as T)} navigatorData={mergeNavigator} />
+    return (
+      <Wrapper
+        ref={componentRef}
+        {...(rest as T)}
+        navigatorData={isFeatureActive ? [...navigatorData, AuthenticationRoute] : navigatorData}
+      />
+    )
   })
 }
 
