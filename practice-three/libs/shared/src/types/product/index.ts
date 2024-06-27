722d930b02b1c542046f74b8fe359357a00d8d16
Toan <92291488+QToan02@users.noreply.github.com>
2024-06-27 17:19:12 +0700

    #45 Declare product type

---
+++ practice-three/libs/shared/src/types/product/index.ts
@@ -0,0 +1,34 @@
+export type TProduct = {
+  id: string
+  name: string
+  image: string
+  price: number
+  discountPercent: number
+  brandName: string
+  rating: number
+  description: string
+  sellerName: string
+  sizes: string[]
+  reviews: TReview[]
+  specifications: TProductSpecification
+}
+
+export type TProductSpecification = {
+  type: string
+  color: string
+  sleeveLength: string
+  patternType: string
+  length: string
+  closure: string
+  liningFabric: string
+  numOfPockets: number
+  hemline: string
+  occasion: string
+}
+
+export type TReview = {
+  rating: number
+  content: string
+  reviewer: string
+  date: Date
+}
