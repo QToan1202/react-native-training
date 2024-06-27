ca2b99df96383de2f269f30eeb6d7616079793c0
Toan <92291488+QToan02@users.noreply.github.com>
2024-06-27 17:26:02 +0700

    #45 Create `ProductCard` component for web app

---
+++ practice-three/libs/product/src/components/ProductCard/ProductCard.tsx
@@ -0,0 +1,99 @@
+import { Card, CardProps, H2, Image, XStack, getTokenValue } from 'tamagui'
+import { StarFull } from '@tamagui/lucide-icons'
+
+import { Text } from '@shared/components'
+import { TProduct } from '@shared/types'
+import { calculateDiscountPrice } from '@shared/utils'
+
+import { placeholderImagePath } from '../../assets/images'
+
+type TOmitProductProps =
+  | 'id'
+  | 'description'
+  | 'sellerName'
+  | 'sizes'
+  | 'reviews'
+  | 'specifications'
+export type ProductCardProps = CardProps & Omit<TProduct, TOmitProductProps>
+
+const ProductCard = ({
+  name,
+  brandName,
+  image,
+  rating,
+  price,
+  discountPercent,
+  ...rest
+}: ProductCardProps) => {
+  return (
+    <Card
+      maxWidth={getTokenValue('$card.width')}
+      maxHeight={getTokenValue('$card.height')}
+      borderRadius={10}
+      backgroundColor="$white"
+      hoverStyle={{
+        cursor: 'pointer',
+        // @ts-expect-error: Resolve css type not working
+        filter: 'brightness(90%)',
+      }}
+      {...rest}
+    >
+      <Image
+        resizeMode="contain"
+        alignSelf="center"
+        source={{
+          width: getTokenValue('$card.width'),
+          height: 300,
+          uri: image,
+        }}
+        defaultSource={{
+          width: getTokenValue('$card.width'),
+          height: 300,
+          uri: placeholderImagePath,
+        }}
+      />
+      <Card.Header paddingHorizontal={21} paddingVertical={10} gap={12}>
+        <H2
+          whiteSpace="nowrap"
+          overflow="hidden"
+          textOverflow="ellipsis"
+          color="$black"
+          fontSize="$5"
+          fontWeight="bold"
+        >
+          {name}
+        </H2>
+        <XStack gap={30}>
+          <Text color="$black" textTransform="capitalize" fontSize="$3">
+            {brandName}
+          </Text>
+          <XStack gap={3}>
+            <Text color="$gray_100" fontSize="$3">
+              {rating}
+            </Text>
+            <StarFull size={18} color="$gray_100" />
+          </XStack>
+        </XStack>
+      </Card.Header>
+      <Card.Footer marginHorizontal={21} marginBottom={12}>
+        <XStack gap={15} alignItems="center">
+          <Text fontSize="$5" fontWeight="bold">
+            Rs.{discountPercent ? calculateDiscountPrice(price, discountPercent) : price}
+          </Text>
+          {discountPercent && (
+            <>
+              <Text textDecorationLine="line-through" fontSize="$3">
+                Rs.{price}
+              </Text>
+              <Text color="$green_100" fontSize="$3" fontWeight="bold">
+                ({discountPercent}% off)
+              </Text>
+            </>
+          )}
+        </XStack>
+      </Card.Footer>
+    </Card>
+  )
+}
+
+export default ProductCard
