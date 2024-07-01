edcc1692ad100ff581964ffb16ba49e7cec00453
Toan <92291488+QToan02@users.noreply.github.com>
2024-07-01 17:30:47 +0700

    Update export directly `StyledText` component instead of `StyleableText`

--- practice-three/libs/shared/src/components/Text/Text.tsx
+++ practice-three/libs/shared/src/components/Text/Text.tsx
@@ -1,11 +1,13 @@
-import StyledText, { StyledTextProps } from './styles'
+import { GetProps, Text as TText, styled } from 'tamagui'
 
-const Text = StyledText.styleable<StyledTextProps>(({ children, ...rest }, ref) => {
-  return (
-    <StyledText ref={ref} {...rest}>
-      {children}
-    </StyledText>
-  )
+const Text = styled(TText, {
+  name: 'Text',
+  tag: 'p',
+  fontWeight: '400',
+  fontFamily: '$body',
+  color: '$color.black',
 })
+
+export type TextProps = GetProps<typeof Text>
 
 export default Text
