9845769e2fa2a5f836f321055a86ed3c69280530
Toan <92291488+QToan02@users.noreply.github.com>
2024-07-01 17:29:13 +0700

    Update `Rating` component styles and props

--- practice-three/libs/shared/src/components/Rating/Rating.tsx
+++ practice-three/libs/shared/src/components/Rating/Rating.tsx
@@ -1,36 +1,51 @@
 import { styled } from 'tamagui'
-import { Radio, RadioItem } from '../Radio'
 import { Star, StarFull } from '@tamagui/lucide-icons'
 import { useId, useState } from 'react'
 
+import { Radio, RadioItem } from '../Radio'
+
 const Item = styled(RadioItem, {
   borderWidth: 0,
-  backgroundColor: '$transparent',
+  backgroundColor: 'transparent',
+  animation: 'fast',
 
   focusStyle: {
     borderWidth: 0,
-    backgroundColor: '$transparent',
+    backgroundColor: 'transparent',
   },
+  hoverStyle: {
+    scale: 1.8,
+    backgroundColor: 'transparent',
+  },
+
+  variants: {
+    disabled: {
+      true: {
+        hoverStyle: {
+          scale: 1,
+        },
+      },
+    },
+  } as const,
+  defaultVariants: { disabled: false },
 })
 
 export type RatingProps = {
+  isDisabled?: boolean
+  defaultValue?: number
   numberOfStarts?: number
 }
 
-const Rating = ({ numberOfStarts = 0 }: RatingProps) => {
+const Rating = ({ isDisabled = false, defaultValue = 0, numberOfStarts = 0 }: RatingProps) => {
   const key = useId()
-  const [rating, setRating] = useState('')
-  const handleChange = (value: string) => setRating(value)
+  const [rating, setRating] = useState<number>(Math.floor(defaultValue))
+  const handleChange = (value: string) => setRating(Number(value))
 
   return (
-    <Radio value={rating} onValueChange={handleChange} flexDirection="row" gap="$1">
+    <Radio value={String(rating)} onValueChange={handleChange} flexDirection="row" gap="$1">
       {[...Array(Math.floor(numberOfStarts) + 1).keys()].slice(1).map((value: number) => (
-        <Item key={key + value} value={value.toString()}>
-          {Number(rating) < value ? (
-            <Star color="$yellow10Light" />
-          ) : (
-            <StarFull color="$yellow10Light" />
-          )}
+        <Item disabled={isDisabled} key={key + value} value={value.toString()}>
+          {rating < value ? <Star color="$yellow10Light" /> : <StarFull color="$yellow10Light" />}
         </Item>
       ))}
     </Radio>
