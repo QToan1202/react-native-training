4af3bfa05dbcac69a86f2f78d4402e6cbcb21b4a
Toan <92291488+QToan02@users.noreply.github.com>
2024-07-01 17:25:05 +0700

    #45 Create `CategoryItem` component

--- practice-three/libs/product/src/assets/images/index.ts
+++ practice-three/libs/product/src/assets/images/index.ts
@@ -2,5 +2,8 @@
 
 export { default as PlaceholderImage } from './placeholder.svg?react'
 export { default as placeholderImagePath } from './placeholder.svg'
+
 export { default as Search } from './search.svg?react'
 export { default as SearchMobile } from './search-mobile.svg?react'
+
+export { default as Arrow } from './arrow.svg?react'
---
+++ practice-three/libs/product/src/components/CategoryItem/CategoryItem.tsx
@@ -0,0 +1,62 @@
+import { ImageURISource } from 'react-native'
+import { Card, CardProps, H2, Image, XStack, YStack, getTokenValue } from 'tamagui'
+
+import { Arrow, placeholderImagePath } from '../../assets/images'
+import { Text } from '@shared/components'
+
+export type CategoryItemProps = CardProps & {
+  title: string
+  image: ImageURISource['uri']
+}
+
+export const CategoryItem = ({ title, image, ...rest }: CategoryItemProps) => {
+  return (
+    <Card
+      maxWidth={getTokenValue('$category.width')}
+      maxHeight={getTokenValue('$category.height')}
+      hoverStyle={{
+        cursor: 'pointer',
+      }}
+      {...rest}
+    >
+      <Card.Header>
+        <YStack>
+          <H2
+            ellipse
+            color="$pure_white"
+            fontSize="$5"
+            fontWeight="bold"
+            textTransform="capitalize"
+          >
+            {title}
+          </H2>
+          <XStack alignItems="center" gap={5}>
+            <Text color="$pure_white" fontSize="$3" fontWeight="bold">
+              Explore
+            </Text>
+            <Arrow />
+          </XStack>
+        </YStack>
+      </Card.Header>
+      <Card.Background $platform-web={{ filter: 'brightness(50%)' }}>
+        <Image
+          resizeMode="cover"
+          alignSelf="center"
+          borderRadius={10}
+          source={{
+            width: getTokenValue('$category.width'),
+            height: getTokenValue('$category.height'),
+            uri: image,
+          }}
+          defaultSource={{
+            width: getTokenValue('$category.width'),
+            height: getTokenValue('$category.height'),
+            uri: placeholderImagePath,
+          }}
+        />
+      </Card.Background>
+    </Card>
+  )
+}
+
+export default CategoryItem
