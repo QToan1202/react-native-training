dea1130ef413d6e1cba7ca6d6a9b51fb43928c3c
Toan <92291488+QToan02@users.noreply.github.com>
2024-07-05 17:06:25 +0700

    Update `Slider` style

--- practice-three/libs/shared/src/components/Slider/Slider.tsx
+++ practice-three/libs/shared/src/components/Slider/Slider.tsx
@@ -8,11 +8,15 @@ const Thumb = styled(Slider.Thumb, {
   size: '$1',
   backgroundColor: '$white',
   borderColor: '$gray_100',
-  borderWidth: 1,
+  borderWidth: 0,
+  focusStyle: {
+    outlineWidth: 0,
+    backgroundColor: '$white',
+  },
 })
 
 const SimpleSlider = (props: SliderProps) => (
-  <Slider min={0} max={100} step={1} {...props}>
+  <Slider min={0} max={100} step={1} marginVertical={14} {...props}>
     <Slider.Track backgroundColor="$gray_50">
       <Slider.TrackActive backgroundColor="$blue_50" />
     </Slider.Track>
