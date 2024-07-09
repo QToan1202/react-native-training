0e56f29cb7c5702eac617da822caa7ef35a55aee
Toan <92291488+QToan02@users.noreply.github.com>
2024-07-09 17:10:15 +0700

    #45 Split filter query and update query key for `useGetProducts` hook

--- practice-three/libs/product/src/hooks/useGetProducts/index.ts
+++ practice-three/libs/product/src/hooks/useGetProducts/index.ts
@@ -2,11 +2,14 @@ import { UseQueryResult, useQuery } from '@tanstack/react-query'
 
 import { TProduct } from '@shared/types'
 import { get } from '@shared/services'
+
 import { STALE_TIMES } from '../../constants'
 
 const useGetProducts = (path: string): UseQueryResult<TProduct[], Error> => {
+  const [, filterQuery] = path.split('?')
+
   return useQuery<TProduct[], Error, TProduct[], string[]>({
-    queryKey: ['products'],
+    queryKey: ['products', filterQuery],
     queryFn: () => get(path),
     staleTime: STALE_TIMES.PRODUCT_INFO,
   })
