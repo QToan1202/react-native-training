2e08090af334593f7abe933deaee194beac2d4e4
Toan <92291488+QToan02@users.noreply.github.com>
2024-07-10 15:58:59 +0700

    Update query key for `useGetProducts` hook

--- practice-three/libs/product/src/hooks/useGetProducts/index.ts
+++ practice-three/libs/product/src/hooks/useGetProducts/index.ts
@@ -2,14 +2,16 @@ import { UseQueryResult, useQuery } from '@tanstack/react-query'
 
 import { TProduct } from '@shared/types'
 import { get } from '@shared/services'
+import { parseURLSearchParams } from '@shared/utils'
 
 import { STALE_TIMES } from '../../constants'
 
 const useGetProducts = (path: string): UseQueryResult<TProduct[], Error> => {
   const [, filterQuery] = path.split('?')
+  const convertedFilterQuery = parseURLSearchParams(filterQuery)
 
-  return useQuery<TProduct[], Error, TProduct[], string[]>({
-    queryKey: ['products', filterQuery],
+  return useQuery<TProduct[], Error, TProduct[], (string | Record<string, unknown>)[]>({
+    queryKey: ['products', convertedFilterQuery],
     queryFn: () => get(path),
     staleTime: STALE_TIMES.PRODUCT_INFO,
   })
