f731313b8ba4e5f5bf98a12dd43be51a42017e9e
Toan <92291488+QToan02@users.noreply.github.com>
2024-07-11 13:50:51 +0700

    Split `useGetProducts` query options

--- practice-three/libs/product/src/hooks/index.ts
+++ practice-three/libs/product/src/hooks/index.ts
@@ -1,2 +1,4 @@
+export * from './useGetProducts'
 export { default as useGetProducts } from './useGetProducts'
+
 export { default as useFindProduct } from './useFindProduct'
--- practice-three/libs/product/src/hooks/useGetProducts/index.ts
+++ practice-three/libs/product/src/hooks/useGetProducts/index.ts
@@ -1,4 +1,4 @@
-import { UseQueryResult, useQuery } from '@tanstack/react-query'
+import { UseQueryResult, queryOptions, useQuery } from '@tanstack/react-query'
 
 import { TProduct } from '@shared/types'
 import { get } from '@shared/services'
@@ -6,15 +6,19 @@ import { parseURLSearchParams } from '@shared/utils'
 
 import { STALE_TIMES } from '../../constants'
 
-const useGetProducts = (path: string): UseQueryResult<TProduct[], Error> => {
+export const getProductsQuery = (path: string) => {
   const [, filterQuery] = path.split('?')
   const convertedFilterQuery = parseURLSearchParams(filterQuery)
 
-  return useQuery<TProduct[], Error, TProduct[], (string | Record<string, unknown>)[]>({
+  return queryOptions<TProduct[], Error, TProduct[], (string | Record<string, unknown>)[]>({
     queryKey: ['products', convertedFilterQuery],
     queryFn: () => get(path),
     staleTime: STALE_TIMES.PRODUCT_INFO,
   })
+}
+
+const useGetProducts = (path: string): UseQueryResult<TProduct[], Error> => {
+  return useQuery(getProductsQuery(path))
 }
 
 export default useGetProducts
