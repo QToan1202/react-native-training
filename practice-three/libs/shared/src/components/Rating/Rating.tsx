a99a42c70083079bac2bd28321c6e8fad76d5d06
Toan <92291488+QToan02@users.noreply.github.com>
2024-07-11 16:27:07 +0700

    Update `color` prop for `Rating` component

--- practice-three/libs/shared/src/components/Rating/Rating.tsx
+++ practice-three/libs/shared/src/components/Rating/Rating.tsx
@@ -1,5 +1,6 @@
 import { styled } from 'tamagui'
 import { Star, StarFull } from '@tamagui/lucide-icons'
+import { GetThemeValueForKey } from '@tamagui/core'
 import { useId, useState } from 'react'
 
 import { Radio, RadioItem } from '../Radio'
@@ -31,12 +32,18 @@ const Item = styled(RadioItem, {
 })
 
 export type RatingProps = {
+  color?: GetThemeValueForKey<'color'>
   isDisabled?: boolean
   defaultValue?: number
   numberOfStarts?: number
 }
 
-const Rating = ({ isDisabled = false, defaultValue = 0, numberOfStarts = 0 }: RatingProps) => {
+const Rating = ({
+  isDisabled = false,
+  defaultValue = 0,
+  numberOfStarts = 0,
+  color = '$yellow10Light',
+}: RatingProps) => {
   const key = useId()
   const [rating, setRating] = useState<number>(Math.floor(defaultValue))
   const handleChange = (value: string) => setRating(Number(value))
@@ -45,7 +52,7 @@ const Rating = ({ isDisabled = false, defaultValue = 0, numberOfStarts = 0 }: Ra
     <Radio value={String(rating)} onValueChange={handleChange} flexDirection="row" gap="$1">
       {[...Array(Math.floor(numberOfStarts) + 1).keys()].slice(1).map((value: number) => (
         <Item disabled={isDisabled} key={key + value} value={value.toString()}>
-          {rating < value ? <Star color="$yellow10Light" /> : <StarFull color="$yellow10Light" />}
+          {rating < value ? <Star color={color} /> : <StarFull color={color} />}
         </Item>
       ))}
     </Radio>
