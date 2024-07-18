17cce8ff52dfdeb516db86883d17f236fa076514
Toan <92291488+QToan02@users.noreply.github.com>
2024-07-18 17:24:43 +0700

    Provide default font size for `Text` component

--- practice-three/libs/shared/src/components/Text/Text.tsx
+++ practice-three/libs/shared/src/components/Text/Text.tsx
@@ -3,6 +3,7 @@ import { GetProps, Text as TText, styled } from 'tamagui'
 const Text = styled(TText, {
   name: 'Text',
   tag: 'p',
+  fontSize: '$true',
   fontWeight: '400',
   fontFamily: '$body',
   color: '$color.black',
