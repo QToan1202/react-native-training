57c8d849a45515328157b36d0be19706890d2276
Toan <92291488+QToan02@users.noreply.github.com>
2024-07-24 14:31:43 +0700

    Update `getWishlistQuery` can include parent resource

--- practice-three/libs/product/src/hooks/useGetWishlist/index.ts
+++ practice-three/libs/product/src/hooks/useGetWishlist/index.ts
@@ -1,15 +1,20 @@
 import { queryOptions } from '@tanstack/react-query'
 
 import { get } from '@shared/services'
-import { TWishlistBase } from '@shared/types'
+import { TWishlistBase, TWishlistExpand } from '@shared/types'
 
 import { STALE_TIMES } from '../../constants'
 
-const getWishlistQuery = (path: string, userId: string) =>
-  queryOptions<TWishlistBase[], Error, TWishlistBase[], string[]>({
+type TWishlistMode<T extends boolean> = T extends true ? TWishlistExpand : TWishlistBase
+
+const getWishlistQuery = <T extends boolean = false>(path: string, userId: string, expand?: T) => {
+  const expandWishlistParams = expand ? { _expand: ['product', 'user'] } : null
+
+  return queryOptions<Array<TWishlistMode<T>>, Error, Array<TWishlistMode<T>>, string[]>({
     queryKey: ['wishlist', userId],
-    queryFn: () => get(path, { params: { userId } }),
+    queryFn: () => get(path, { params: { userId, ...expandWishlistParams } }),
     staleTime: STALE_TIMES.USER_WISHLIST,
   })
+}
 
 export default getWishlistQuery
