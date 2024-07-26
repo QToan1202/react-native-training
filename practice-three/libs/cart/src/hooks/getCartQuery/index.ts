05f784957e5b3773939d348999f4afdc84b672f7
Toan <92291488+QToan02@users.noreply.github.com>
2024-07-26 14:40:28 +0700

    Fix `getCartQuery` to get base on current user

--- practice-three/libs/cart/src/hooks/getCartQuery/index.ts
+++ practice-three/libs/cart/src/hooks/getCartQuery/index.ts
@@ -1,14 +1,14 @@
 import { queryOptions } from '@tanstack/react-query'
 
-import { find } from '@shared/services'
+import { get } from '@shared/services'
 import { TCart } from '@shared/types'
 
 import { STALE_TIMES } from '../../constants'
 
 const getCartQuery = (path: string, userId: string) =>
-  queryOptions<TCart, Error, TCart, string[]>({
+  queryOptions<TCart[], Error, TCart[], string[]>({
     queryKey: ['carts', userId],
-    queryFn: () => find(`${path}/${userId}`),
+    queryFn: () => get(path, { params: { userId } }),
     staleTime: STALE_TIMES.CART,
   })
 
