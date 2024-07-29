f8249afba3fa87d73359d6e09969d3b67789a6aa
Toan <92291488+QToan02@users.noreply.github.com>
2024-07-29 16:46:29 +0700

    Fix return type in `findPromoCodeQuery` function

--- practice-three/libs/cart/src/hooks/findPromoCodeQuery/index.ts
+++ practice-three/libs/cart/src/hooks/findPromoCodeQuery/index.ts
@@ -1,13 +1,13 @@
 import { queryOptions } from '@tanstack/react-query'
 
 import { get } from '@shared/services'
-import { TProduct } from '@shared/types'
+import { TOffer } from '@shared/types'
 
 import { STALE_TIMES } from '../../constants'
 
 const findPromoCodeQuery = (path: string, code: string) =>
-  queryOptions<TProduct[], Error, TProduct[], string[]>({
-    queryKey: ['product', code],
+  queryOptions<TOffer[], Error, TOffer[], string[]>({
+    queryKey: ['offer', code],
     queryFn: () => get(path, { params: { code } }),
     staleTime: STALE_TIMES.PROMO_CODE,
     enabled: !!code.trim().length,
