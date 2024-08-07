b8ec9bb2a79eae5e7c48ae38f801284ef229d27b
Toan <92291488+QToan02@users.noreply.github.com>
2024-08-07 15:55:47 +0700

    #47 Declare constants for all address fields

--- practice-three/libs/checkout/src/constants/index.ts
+++ practice-three/libs/checkout/src/constants/index.ts
@@ -1,5 +1,142 @@
+import { UseControllerProps } from 'react-hook-form'
+
+import { TDelimiterCase } from '@shared/utils'
+
+import { TAddressForm } from '../types'
+
 export const STEPPER_LABELS = ['Cart', 'Address', 'Payment', 'Summary']
 
 export const STALE_TIMES = {
   ADDRESS: 24 * 60 * 60 * 1000, // 1 day
+}
+
+type TAddressFormConstant = {
+  [K in keyof TAddressForm as Uppercase<TDelimiterCase<K, '_'>>]: {
+    label: keyof TAddressForm
+    title: string
+    rules?: UseControllerProps['rules']
+  }
+}
+
+export const ADDRESS_FORM: TAddressFormConstant = {
+  COUNTRY: {
+    label: 'country',
+    title: 'Country',
+    rules: {
+      required: {
+        value: true,
+        message: 'Country field is require. Please select value!',
+      },
+    },
+  },
+  FIRST_NAME: {
+    label: 'firstName',
+    title: 'First Name',
+    rules: {
+      required: {
+        value: true,
+        message: 'First name field is require. Please enter any value!',
+      },
+      maxLength: {
+        value: 30,
+        message: 'Your value you just entered is too long, maybe try shorter value!',
+      },
+    },
+  },
+  LAST_NAME: {
+    label: 'lastName',
+    title: 'Last Name',
+    rules: {
+      required: {
+        value: true,
+        message: 'Last name field is require. Please enter any value!',
+      },
+      maxLength: {
+        value: 30,
+        message: 'Your value you just entered is too long, maybe try shorter value!',
+      },
+    },
+  },
+  ADDRESS: {
+    label: 'address',
+    title: 'Street Address',
+    rules: {
+      required: {
+        value: true,
+        message: 'Address field is require. Please enter any value!',
+      },
+      maxLength: {
+        value: 50,
+        message: 'Your value you just entered is too long, maybe try shorter value!',
+      },
+    },
+  },
+  OPTIONAL_ADDRESS: {
+    label: 'optionalAddress',
+    title: 'Street Address 2 (Optional)',
+  },
+  CITY: {
+    label: 'city',
+    title: 'City',
+    rules: {
+      required: {
+        value: true,
+        message: 'City field is require. Please enter any value!',
+      },
+      maxLength: {
+        value: 20,
+        message: 'Your value you just entered is too long, maybe try shorter value!',
+      },
+    },
+  },
+  STATE: {
+    label: 'state',
+    title: 'State/Province/Region',
+    rules: {
+      required: {
+        value: true,
+        message: 'State/Province/Region field is require. Please enter any value!',
+      },
+      maxLength: {
+        value: 20,
+        message: 'Your value you just entered is too long, maybe try shorter value!',
+      },
+    },
+  },
+  ZIP_CODE: {
+    label: 'zipCode',
+    title: 'Zip Code',
+    rules: {
+      required: {
+        value: true,
+        message: 'Zip Code field is require. Please enter any value!',
+      },
+      pattern: {
+        value: /^\d+$/,
+        message: 'Zip Code must have only number.',
+      },
+      maxLength: {
+        value: 6,
+        message: 'Your value you just entered is too long, maybe try shorter value!',
+      },
+    },
+  },
+  PHONE: {
+    label: 'phone',
+    title: 'Phone Number',
+    rules: {
+      required: {
+        value: true,
+        message: 'Phone field is require. Please enter any value!',
+      },
+      pattern: {
+        value: /^\d+$/,
+        message: 'Phone must have only have number. Please re-check your phone!',
+      },
+      maxLength: {
+        value: 15,
+        message: 'Your value you just entered is too long, maybe try shorter value!',
+      },
+    },
+  },
 }
--- practice-three/libs/shared/src/utils/common/index.ts
+++ practice-three/libs/shared/src/utils/common/index.ts
@@ -92,3 +92,21 @@ export const convertQueryStr = (query: string) => {
 export type TResolveLoaderReturn<T extends (...args: any) => any> = Awaited<
   ReturnType<ReturnType<T>>
 >
+
+/**
+ * Type to convert a string from camelCase to a delimited case (e.g., snake_case).
+ *
+ * @template S - The input string type to convert.
+ * @template Delimiter - The delimiter to insert before uppercase letters.
+ *
+ * ```ts
+ * type SnakeCaseString = TDelimiterCase<'thisIsCamelCase', '_'>;  // 'this_is_camel_case'
+ * type KebabCaseString = TDelimiterCase<'thisIsCamelCase', '-'>;  // 'this-is-camel-case'
+ * ```
+ */
+export type TDelimiterCase<
+  S extends string,
+  Delimiter extends string
+> = S extends `${infer T}${infer U}`
+  ? `${T extends Lowercase<T> ? '' : Delimiter}${Lowercase<T>}${TDelimiterCase<U, Delimiter>}`
+  : S
