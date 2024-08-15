82653db6c7a0a9dd3b374fa39850dd17ad3ce4b0
Toan <92291488+QToan02@users.noreply.github.com>
2024-08-15 17:33:02 +0700

    Create Order Item Skeleton for native environment

---
+++ practice-three/libs/checkout/src/components/OrderItem/OrderItemSkeleton.native.tsx
@@ -0,0 +1,31 @@
+import { XStack, XStackProps, YStack } from 'tamagui'
+
+import { Skeleton } from '@shared/components'
+
+type OrderItemSkeletonProps = XStackProps
+
+const OrderItemSkeleton = ({ ...props }: OrderItemSkeletonProps) => {
+  return (
+    <XStack
+      justifyContent="space-between"
+      padding={15}
+      borderBottomWidth={1}
+      borderTopWidth={1}
+      borderColor="$pale"
+      {...props}
+    >
+      <XStack gap={15}>
+        <Skeleton width={88} height={88} />
+        <YStack justifyContent="space-between">
+          <YStack gap={5}>
+            <Skeleton width={120} height={16} />
+            <Skeleton width={120} height={16} />
+          </YStack>
+          <Skeleton width={120} height={24} />
+        </YStack>
+      </XStack>
+    </XStack>
+  )
+}
+
+export default OrderItemSkeleton
