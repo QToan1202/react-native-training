682de6fdf52a5d71ecf60ef7c4a5bc8de7c41d59
Toan <92291488+QToan02@users.noreply.github.com>
2024-08-16 16:56:07 +0700

    #47 Create navigation for Checkout feat

---
+++ practice-three/libs/checkout/src/navigation/router.native.tsx
@@ -0,0 +1,25 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+import { CheckoutStack } from '@shared/types'
+
+import {
+  AddAddressScreen,
+  AddCardScreen,
+  AddressScreen,
+  OrderScreen,
+  PaymentScreen,
+} from '../screens'
+
+const Stack = createNativeStackNavigator<CheckoutStack>()
+
+const CheckoutStack = () => (
+  <Stack.Navigator screenOptions={{ headerShown: false }}>
+    <Stack.Screen name="AddAddress" component={AddAddressScreen} />
+    <Stack.Screen name="AddPayment" component={AddCardScreen} />
+    <Stack.Screen name="Address" component={AddressScreen} />
+    <Stack.Screen name="Order" component={OrderScreen} />
+    <Stack.Screen name="Payment" component={PaymentScreen} />
+  </Stack.Navigator>
+)
+
+export default CheckoutStack
