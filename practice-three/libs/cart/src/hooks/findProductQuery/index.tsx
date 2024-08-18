cd2133eca622e3839b6f660d66c61184921257d3
Toan <92291488+QToan02@users.noreply.github.com>
2024-08-18 11:58:38 +0700

    Update dependent query useFindProducts

--- practice-three/libs/cart/src/hooks/findProductQuery/index.tsx
+++ practice-three/libs/cart/src/hooks/findProductQuery/index.tsx
@@ -1,5 +1,5 @@
 import { useMemo, useRef } from 'react'
-import { queryOptions, useQueries, UseQueryResult, useSuspenseQuery } from '@tanstack/react-query'
+import { queryOptions, useQueries, useQuery, UseQueryResult } from '@tanstack/react-query'
 
 import { find } from '@practice-three/services'
 import { TProduct, TCartItem, TCart } from '@practice-three/types'
@@ -18,8 +18,13 @@ export const findProductQuery = (path: string, id: string) =>
 export const useFindProducts = (): [boolean, TCartItem[]] => {
   const user = useAuthStore((state) => state.user)
   const isFetchingProduct = useRef<boolean>(true)
-  const { data: carts } = useSuspenseQuery(getCartQuery('carts', user?.id || ''))
-  const firstCartItem: TCart | undefined = carts.at(0)
+  const { data: carts, isSuccess: isGetCartsSuccess } = useQuery(
+    getCartQuery('carts', user?.id || '')
+  )
+  const firstCartItem: TCart | undefined = useMemo(
+    () => (isGetCartsSuccess ? carts.at(0) : undefined),
+    [carts, isGetCartsSuccess]
+  )
   const getProductsQuery = useQueries({
     queries: firstCartItem
       ? Object.keys(firstCartItem.items).map((item) => findProductQuery('/products', item))
