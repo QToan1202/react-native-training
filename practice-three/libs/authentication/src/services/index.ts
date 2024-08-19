c003cfa3f20ebde04f1c96a5c5885a780984f541
Toan <92291488+QToan02@users.noreply.github.com>
2024-08-19 08:28:10 +0700

    Check account type before adding user to database

--- practice-three/libs/authentication/src/services/index.ts
+++ practice-three/libs/authentication/src/services/index.ts
@@ -1,20 +1,57 @@
-import { add, get } from '@practice-three/services'
-import { TUser } from '@practice-three/types'
+import bcrypt from 'bcrypt'
 
-export const register = async (path: string, user: TUser): Promise<TUser> => {
-  const { email } = user
-  const users: TUser[] = await get(path, { params: { email } })
+import { add, get } from '@practice-three/services'
+import { TRegisterForm, TUser } from '@practice-three/types'
+
+import { REGEX } from '../constants'
+
+export const register = async (path: string, data: TRegisterForm): Promise<TUser> => {
+  const { account, password, name } = data
+  const isEmail = REGEX.EMAIL.test(account)
+  const transformAccount = isEmail ? { email: account } : { phone: account }
+  const users: TUser[] = await get(path, { params: transformAccount })
+  const expectReturnUser: TUser | undefined = users.at(0)
 
   // Check register Email exist
-  if (users.length) throw Error('This email already use by other client')
+  if (expectReturnUser)
+    throw Error('This information already use by other client, try will other account')
 
-  return add<TUser>(path, user)
+  const saltRounds = 10
+  const salt = await bcrypt.genSalt(saltRounds)
+  const hashedPassword = await bcrypt.hash(password, salt)
+
+  const userData: Omit<TUser, 'id'> = isEmail
+    ? {
+        name,
+        email: account,
+        phone: '',
+        password: hashedPassword,
+      }
+    : {
+        name,
+        email: '',
+        phone: account,
+        password: hashedPassword,
+      }
+
+  return add<TUser>(path, userData)
 }
 
-export const login = async (path: string, email: string, password: string): Promise<TUser> => {
-  const users: TUser[] = await get(path, { params: { email, password } })
+export const login = async (path: string, account: string, password: string): Promise<TUser> => {
+  const isEmail = REGEX.EMAIL.test(account)
+  const transformData = isEmail
+    ? {
+        email: account,
+      }
+    : { phone: account }
+  const users: TUser[] = await get(path, { params: transformData })
+  const expectReturnUser: TUser | undefined = users.at(0)
 
-  if (!users.length) throw Error('Login fail, check email or password')
+  if (!expectReturnUser) throw Error('Login fail, check email or password')
 
-  return users[0]
+  const isMatchPassword = await bcrypt.compare(password, expectReturnUser.password)
+
+  if (!isMatchPassword) throw Error('Login fail, check email or password')
+
+  return expectReturnUser
 }
--- practice-three/libs/shared/src/types/user/index.ts
+++ practice-three/libs/shared/src/types/user/index.ts
@@ -1,6 +1,5 @@
 export type TUser = {
   id: string
-  account: string
   name: string
   email: string
   phone: string
