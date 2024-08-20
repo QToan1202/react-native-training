a897a1412ded7d415e2ec9fe8cd0f357dc8fe971
Toan <92291488+QToan02@users.noreply.github.com>
2024-08-20 10:17:31 +0700

    Fix request module error in Image Gallery component

--- practice-three/libs/shared/src/components/ImageGallery/styles.ts
+++ practice-three/libs/shared/src/components/ImageGallery/styles.ts
@@ -1,5 +1,5 @@
 import { isWeb } from 'tamagui'
 
-const styles = isWeb ? require('./ImageGallery.module.css') : null
+const styles = isWeb ? await import('./ImageGallery.module.css') : null
 
 export default styles
