89923abe3fabcbb70c87b539f98cd03e7928c8d3
Toan <92291488+QToan02@users.noreply.github.com>
2024-08-21 15:41:06 +0700

    #49 Create Layout for web app

---
+++ practice-three/apps/web/src/layout/Root/Root.tsx
@@ -0,0 +1,14 @@
+import { Outlet } from 'react-router-dom'
+import { Footer, Header } from '../../components'
+
+const Root = () => {
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
+  )
+}
+
+export default Root
