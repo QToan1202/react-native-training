e4a536913428e030530e5c3c281b00086fe0a047
Toan <92291488+QToan02@users.noreply.github.com>
2024-08-21 17:02:21 +0700

    Create NotFound screen

---
+++ practice-three/libs/shared/src/screens/NotFound/NotFound.tsx
@@ -0,0 +1,19 @@
+import { Heading, YStack } from 'tamagui'
+
+import { Text } from '../../components'
+
+const NotFound = () => (
+  <YStack justifyContent="center" alignItems="center" gap={16} backgroundColor="$white" fullscreen>
+    <Heading color="$pure_black" fontSize={80} fontWeight="700">
+      404
+    </Heading>
+    <Heading color="$pure_black" fontSize="$5" fontWeight="500">
+      Not Found
+    </Heading>
+    <Text color="$pure_black" fontSize="$3">
+      This resource requested could not be found on this server!
+    </Text>
+  </YStack>
+)
+
+export default NotFound
