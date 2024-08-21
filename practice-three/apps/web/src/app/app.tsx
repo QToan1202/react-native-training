9f90e8392ae3c73bb573e966502e6338ccd3c5f2
Toan <92291488+QToan02@users.noreply.github.com>
2024-08-21 17:21:30 +0700

    Add Root Layout for web app

--- practice-three/apps/web/src/app/app.tsx
+++ practice-three/apps/web/src/app/app.tsx
@@ -1,13 +1,28 @@
 import { ReactNode } from 'react'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, RouteObject, RouterProvider } from 'react-router-dom'
 
 import { featureShell } from '@practice-three/shell'
 import { withAuth } from '@practice-three/features/authentication'
 import { withProduct } from '@practice-three/features/product'
 import { THOCsProps } from '@practice-three/types'
 import { withCheckout } from '@practice-three/features/checkout'
+import { NotFoundScreen } from '@practice-three/screens'
+
+import { RootLayout } from '../layout'
 
 const INIT_NAVIGATOR_DATA: THOCsProps['navigatorData'] = []
+const routerLayout: RouteObject[] = [
+  {
+    path: '/',
+    element: <RootLayout />,
+    children: [
+      {
+        path: '*',
+        element: <NotFoundScreen />,
+      },
+    ],
+  },
+]
 
 const initFeatures = featureShell(import.meta.env.VITE_FEATURES)
 const BaseApp = ({
@@ -19,7 +34,13 @@ const WrapHOC = withCheckout(withProduct(withAuth(BaseApp)))
 const App = () => (
   <WrapHOC category={initFeatures} navigatorData={INIT_NAVIGATOR_DATA}>
     {(props) => {
-      const router = createBrowserRouter(props.navigatorData)
+      const getLayoutRoute = routerLayout.at(0)
+
+      if (!getLayoutRoute || !getLayoutRoute.children) throw new Error('Missing base route data')
+
+      const mergeChildren = [...getLayoutRoute.children, ...props.navigatorData]
+      getLayoutRoute.children = mergeChildren
+      const router = createBrowserRouter(routerLayout)
 
       return <RouterProvider router={router} />
     }}
