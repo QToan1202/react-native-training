daa4598794c66cce7dfbb60134e5330bb5ecc433
Toan <92291488+QToan02@users.noreply.github.com>
2024-08-23 14:26:28 +0700

    Add GoBack button on native platform in NotFound screen

--- practice-three/libs/shared/src/screens/NotFound/NotFound.tsx
+++ practice-three/libs/shared/src/screens/NotFound/NotFound.tsx
@@ -1,19 +1,28 @@
-import { Heading, YStack } from 'tamagui'
+import { Heading, isWeb, YStack } from 'tamagui'
 
-import { Text } from '../../components'
+import { RootStackScreenProps } from '../../types'
 
-const NotFound = () => (
-  <YStack flex={1} justifyContent="center" alignItems="center" gap={16} backgroundColor="$white">
-    <Heading color="$pure_black" fontSize={80} fontWeight="700">
-      404
-    </Heading>
-    <Heading color="$pure_black" fontSize="$5" fontWeight="500">
-      Not Found
-    </Heading>
-    <Text color="$pure_black" fontSize="$3">
-      This resource requested could not be found on this server!
-    </Text>
-  </YStack>
-)
+import { Button, Text } from '../../components'
+
+type NotFoundScreenProps = RootStackScreenProps<'NotFound'>
+
+const NotFound = ({ navigation }: NotFoundScreenProps) => {
+  const handleGoBack = () => navigation.goBack()
+
+  return (
+    <YStack flex={1} justifyContent="center" alignItems="center" gap={16} backgroundColor="$white">
+      <Heading color="$pure_black" fontSize={80} fontWeight="700">
+        404
+      </Heading>
+      <Heading color="$pure_black" fontSize="$5" fontWeight="500">
+        Not Found
+      </Heading>
+      <Text color="$pure_black" fontSize="$3">
+        This resource requested could not be found on this server!
+      </Text>
+      {!isWeb && <Button title="go back" onPress={handleGoBack} />}
+    </YStack>
+  )
+}
 
 export default NotFound
