d084fef495fa3fded4c5c29b2d319e5a4d2e1061
Toan <92291488+QToan02@users.noreply.github.com>
2024-09-04 16:37:54 +0700

    Fix render Carousel error in Home page

--- practice-three/apps/web/src/pages/Home/Home.tsx
+++ practice-three/apps/web/src/pages/Home/Home.tsx
@@ -1,6 +1,6 @@
 import { Heading, ScrollView, XStack, YStack } from 'tamagui'
 
-import { Carousel } from '@practice-three/components'
+import { Carousel, Image } from '@practice-three/components'
 
 import {
   AboutUs,
@@ -21,6 +21,16 @@ const Home = () => (
         'https://images.dog.ceo/breeds/husky/n02110185_14479.jpg',
         'https://images.dog.ceo/breeds/setter-english/n02100735_4040.jpg',
       ]}
+      renderItem={({ item }) => (
+        <Image
+          resizeMode="cover"
+          source={{
+            uri: item,
+            width: 400,
+            height: 300,
+          }}
+        />
+      )}
     />
     <ScrollView contentContainerStyle={{ flex: 1, gap: 25 }}>
       <Heading color="$black" fontSize="$6" fontWeight="700">
