4d78dea4a2741e713fdad37fdbba1ff9ac46999a
Toan <92291488+QToan02@users.noreply.github.com>
2024-09-05 16:37:44 +0700

    Fix Skeleton component animation not working on native platform

--- practice-three/libs/shared/src/components/Skeleton/SkeletonIndicator.tsx
+++ practice-three/libs/shared/src/components/Skeleton/SkeletonIndicator.tsx
@@ -1,24 +1,62 @@
-import { forwardRef } from 'react'
+import { forwardRef, useEffect } from 'react'
 import { StyleSheet } from 'react-native'
-import Animated from 'react-native-reanimated'
-import { LinearGradient, LinearGradientProps } from 'expo-linear-gradient'
-import { getTokenValue } from 'tamagui'
+import Animated, {
+  Easing,
+  ReduceMotion,
+  useAnimatedStyle,
+  useSharedValue,
+  withDelay,
+  withRepeat,
+  withTiming,
+} from 'react-native-reanimated'
+import { LinearGradient, LinearGradientProps } from 'react-native-linear-gradient'
+import { getTokenValue, useWindowDimensions } from 'tamagui'
 
 export type SkeletonIndicatorProps = Omit<LinearGradientProps, 'colors'>
 
 const SkeletonIndicator = forwardRef<LinearGradient, SkeletonIndicatorProps>(
-  (props, skeletonRef) => (
-    <LinearGradient
-      ref={skeletonRef}
-      style={StyleSheet.absoluteFill}
-      start={[0, 0]}
-      end={[1, 0]}
-      colors={['transparent', getTokenValue('$color.skeletonIndicator'), 'transparent']}
-      {...props}
-    />
-  )
+  (props, skeletonRef) => {
+    const { width } = useWindowDimensions()
+    const sv = useSharedValue<number>(-width)
+    const animatedStyle = useAnimatedStyle(() => ({
+      transform: [{ translateX: sv.value }],
+    }))
+
+    useEffect(() => {
+      sv.value = withDelay(
+        500,
+        withRepeat(
+          withTiming(width, {
+            duration: 1500,
+            easing: Easing.linear,
+            reduceMotion: ReduceMotion.System,
+          }),
+          -1
+        )
+      )
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+
+    return (
+      <Animated.View style={[styles.container, animatedStyle]}>
+        <LinearGradient
+          ref={skeletonRef}
+          style={StyleSheet.absoluteFill}
+          start={{ x: 0, y: 0 }}
+          end={{ x: 1, y: 0 }}
+          colors={['transparent', getTokenValue('$color.skeletonIndicator'), 'transparent']}
+          {...props}
+        />
+      </Animated.View>
+    )
+  }
 )
 
-const AnimatedSkeletonIndicator = Animated.createAnimatedComponent(SkeletonIndicator)
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: '100%',
+  },
+})
 
-export default AnimatedSkeletonIndicator
+export default SkeletonIndicator
